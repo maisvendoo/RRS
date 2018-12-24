de8ac3e77b6072e78e271a7eaf0d994cec21d183
maisvendoo <maisvendoo@gmail.com>
2018-12-24 13:31:25 +0300

    Textures in train

--- viewer/viewer/src/vehicle-loader.cpp
+++ viewer/viewer/src/vehicle-loader.cpp
@@ -6,6 +6,39 @@
 #include    <osgDB/FileUtils>
 #include    <osgDB/FileNameUtils>
 #include    <osgDB/ReadFile>
+#include    <osg/Texture2D>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+osg::Texture2D *createTexture(const std::string &path)
+{
+    osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
+
+    std::string fileName = osgDB::findDataFile(path);
+
+    if (fileName.empty())
+        return nullptr;
+
+    osg::ref_ptr<osg::Image> image = osgDB::readImageFile(fileName);
+
+    if (!image.valid())
+        return nullptr;
+
+    std::string ext = osgDB::getLowerCaseFileExtension(fileName);
+
+    if (ext == "tga")
+    {
+        image->flipVertical();
+    }
+
+    texture->setImage(image.get());
+    texture->setWrap(osg::Texture2D::WRAP_T, osg::Texture::REPEAT);
+    texture->setWrap(osg::Texture2D::WRAP_S, osg::Texture::REPEAT);
+    texture->setUnRefImageDataAfterApply(true);
+
+    return texture.release();
+}
 
 //------------------------------------------------------------------------------
 //
@@ -30,11 +63,31 @@ osg::Group *loadVehicle(const std::string &configPath)
 
     osg::ref_ptr<osg::Group> group = new osg::Group;
 
+    osg::ref_ptr<osg::Node> model;
+
     if (!modelPath.empty())
     {
         modelPath = fs.toNativeSeparators(modelPath);
-        osg::ref_ptr<osg::Node> model = osgDB::readNodeFile(modelPath);
+        model = osgDB::readNodeFile(modelPath);
         group->addChild(model.get());
+    }
+
+    if (!textureName.empty())
+    {
+        std::string tex_path = fs.combinePath(fs.getVehicleTexturesDir(), textureName);
+        osg::ref_ptr<osg::Texture2D> texture = createTexture(tex_path);
+
+        if (texture.valid())
+        {
+            model->getOrCreateStateSet()->setTextureAttributeAndModes(0, texture.get());
+
+            std::string ext = osgDB::getLowerCaseFileExtension(tex_path);
+
+            if (ext == "tga")
+                model->getOrCreateStateSet()->setRenderingHint(osg::StateSet::TRANSPARENT_BIN);
+            else
+                model->getOrCreateStateSet()->setRenderingHint(osg::StateSet::OPAQUE_BIN);
+        }
     }
 
     return group.release();
