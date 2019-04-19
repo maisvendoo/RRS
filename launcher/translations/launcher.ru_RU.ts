060f5a4a2c1db12d3a85ff4c6f48ebb0ff67e1ef
maisvendoo <maisvendoo@gmail.com>
2019-04-19 07:44:52 +0300

    CheckBox for direction selection

--- launcher/translations/launcher.ru_RU.ts
+++ launcher/translations/launcher.ru_RU.ts
@@ -19,44 +19,53 @@
         <translation>Выбор маршрута</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="105"/>
+        <location filename="../forms/mainwindow.ui" line="111"/>
         <source>Ordinate, m</source>
         <translation>Ордината, м</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="131"/>
         <source>Revert</source>
+        <translation type="vanished">Обратно</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="141"/>
+        <source>Forward</source>
+        <translation>Туда</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="146"/>
+        <source>Backward</source>
         <translation>Обратно</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="156"/>
+        <location filename="../forms/mainwindow.ui" line="172"/>
         <source>Route description</source>
         <translation>Описание маршрута</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="202"/>
-        <location filename="../forms/mainwindow.ui" line="309"/>
-        <location filename="../src/mainwindow.cpp" line="158"/>
+        <location filename="../forms/mainwindow.ui" line="218"/>
+        <location filename="../forms/mainwindow.ui" line="325"/>
+        <location filename="../src/mainwindow.cpp" line="163"/>
         <source>No screenshot</source>
         <translation>Нет скриншота</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="218"/>
+        <location filename="../forms/mainwindow.ui" line="234"/>
         <source>Change train</source>
         <translation>Выбор поезда</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="263"/>
+        <location filename="../forms/mainwindow.ui" line="279"/>
         <source>Train description</source>
         <translation>Описание поезда</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="348"/>
+        <location filename="../forms/mainwindow.ui" line="364"/>
         <source>Start</source>
         <translation>Старт</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="360"/>
+        <location filename="../forms/mainwindow.ui" line="376"/>
         <source>Graphics settings</source>
         <translation>Настройки графики</translation>
     </message>
