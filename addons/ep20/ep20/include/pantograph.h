7ef8f010a13ccc4665c59b9ec347f5e42296573f
MrJecson <balashov.97@mail.ru>
2019-07-30 16:38:37 +0300

    Создан каркас класса для ЭП20. Начата разработка токопремника!

---
+++ addons/ep20/ep20/include/pantograph.h
@@ -0,0 +1,70 @@
+#ifndef PANTOGRAPH_H
+#define PANTOGRAPH_H
+
+#include    "device.h"
+
+//------------------------------------------------------------------------------
+// Токоприемник пантографного типа
+//------------------------------------------------------------------------------
+class Pantograph : public Device
+{
+
+public:
+
+    /// Конструктор
+    Pantograph(QString config_path, QObject *parent = Q_NULLPTR);
+
+    /// Деструктор
+    ~Pantograph();
+
+    /// Установить состояние
+    void setState(bool state);
+
+    /// Установить напряжение в кс
+    void setUks(double Uks);
+
+    /// Получить высоту
+    double getHeight() const;
+
+    /// Получить напряжение на выходе
+    double getUout() const;
+
+private:     
+
+    /// Напряжение в контактной сети
+    double Uks;
+
+    /// Напряжение на выходе токоприемника
+    double Uout;
+
+    /// Состояние токоприемника (true - подъем, false - опускание)
+    bool state;
+    bool old_state;
+
+    /// Максимальная высота подъема (в относительном выражении)
+    double  max_height;
+
+    /// Время подъема/опускания ТП
+    double  motion_time;
+
+    enum KindCurrent
+    {
+        NOT_SPECIFIED, // Род тока неопредлен
+        ALTERNATING_CURRENT, // Переменный ток
+        DIRECT_CURRENT // Постоянный ток
+    };
+
+    double RTout;
+
+
+    void preStep(state_vector_t &Y, double t);
+
+
+    void ode_system(const state_vector_t &Y, state_vector_t &dYdt, double t);
+
+
+    void load_config(CfgReader &cfg);
+
+};
+
+#endif // PANTOGRAPH_H
---
+++ addons/ep20/ep20/src/pantograph.cpp
@@ -0,0 +1,54 @@
+#include    "pantograph.h"
+
+//------------------------------------------------------------------------------
+// Конструктор класса
+//------------------------------------------------------------------------------
+Pantograph::Pantograph(QString config_path, QObject *parent) : Device(parent)
+  , Uks(0.0)
+  , Uout(0.0)
+  , state(false)
+  , old_state(false)
+  , max_height(1.0)
+  , motion_time(4.0)
+  , RTout(0)
+
+{
+//    load_config(config_path);
+}
+
+
+
+Pantograph::~Pantograph()
+{
+
+}
+
+
+void Pantograph::setState(bool state)
+{
+    old_state = this->state;
+    this->state = state;
+}
+
+
+void Pantograph::preStep(state_vector_t &Y, double t)
+{
+
+}
+
+void Pantograph::ode_system(const state_vector_t &Y,
+                            state_vector_t &dYdt,
+                            double t)
+{
+    Q_UNUSED(t)
+
+    double ref_height = static_cast<double>(state) * max_height;
+
+
+}
+
+
+void Pantograph::load_config(CfgReader &cfg)
+{
+//    Q_UNUSED(cfg)
+}
