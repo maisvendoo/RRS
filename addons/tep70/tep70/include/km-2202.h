f9362d6bcdf7a0e21a6ef7a45a143e70c7cffb10
maisvendoo <maisvendoo@gmail.com>
2019-11-13 16:50:03 +0300

    ТЭП70

---
+++ addons/tep70/tep70/include/km-2202.h
@@ -0,0 +1,80 @@
+//------------------------------------------------------------------------------
+//
+//      Контроллер машиниста КМ 2202
+//
+//
+//------------------------------------------------------------------------------
+#ifndef     KM_2202_H
+#define     KM_2202_H
+
+#include    "device.h"
+#include    "timer.h"
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+class ControllerKM2202 : public Device
+{
+public:
+
+    ControllerKM2202(QObject *parent = Q_NULLPTR);
+
+    ~ControllerKM2202();
+
+    /// Вернуть текущую позицию штурвала
+    int getPositionNumber() const { return ms_position; }
+
+    /// Вернуть текущую позицию реверсивки
+    int getReversState() const { return rs_position; }
+
+private:
+
+    enum
+    {
+        MS_ZERO = 0,
+        MS_MAX_POSITION = 15
+    };
+
+    enum
+    {
+        RS_FORWARD = 1,
+        RS_ZERO = 0,
+        RS_BACKWARD = -1
+    };
+
+    double  ms_delay;
+
+    double  rs_delay;
+
+    /// Положение главного вала
+    int     ms_position;
+
+    /// Направление вращения главного вала
+    int     ms_dir;
+
+    /// Положение реверсивного вала
+    int     rs_position;
+
+    /// Направление вращения реверсивного вала
+    int     rs_dir;
+
+    Timer   main_shaft_timer;
+
+    Timer   revers_shaft_timer;
+
+    void preStep(state_vector_t &Y, double t);
+
+    void ode_system(const state_vector_t &Y, state_vector_t &dYdt, double t);
+
+    void load_config(CfgReader &cfg);
+
+    void stepKeysControl(double t, double dt);
+
+private slots:
+
+    void slotRotateMainShaft();
+
+    void slotRotateReversShaft();
+};
+
+#endif // KM2202_H
---
+++ addons/tep70/tep70/src/km-2202.cpp
@@ -0,0 +1,144 @@
+#include    "km-2202.h"
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+ControllerKM2202::ControllerKM2202(QObject *parent) : Device(parent)
+  , ms_delay(0.2)
+  , rs_delay(0.2)
+  , ms_position(MS_ZERO)
+  , ms_dir(0)
+  , rs_position(RS_BACKWARD)
+  , rs_dir(0)
+{
+    main_shaft_timer.firstProcess(true);
+    connect(&main_shaft_timer, &Timer::process,
+            this, &ControllerKM2202::slotRotateMainShaft);
+
+    revers_shaft_timer.firstProcess(true);
+    connect(&revers_shaft_timer, &Timer::process,
+            this, &ControllerKM2202::slotRotateReversShaft);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+ControllerKM2202::~ControllerKM2202()
+{
+
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControllerKM2202::preStep(state_vector_t &Y, double t)
+{
+
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControllerKM2202::ode_system(const state_vector_t &Y,
+                                  state_vector_t &dYdt,
+                                  double t)
+{
+
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControllerKM2202::load_config(CfgReader &cfg)
+{
+    QString secName = "Device";
+
+    int timeout = 200;
+
+    if (cfg.getInt(secName, "MainShaftDelay", timeout))
+    {
+        ms_delay = static_cast<double>(timeout) / 1000.0;
+    }
+
+    main_shaft_timer.setTimeout(ms_delay);
+
+    timeout = 200;
+
+    if (cfg.getInt(secName, "ReversShaftDelay", timeout))
+    {
+        rs_delay = static_cast<double>(timeout) / 1000.0;
+    }
+
+    revers_shaft_timer.setTimeout(rs_delay);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControllerKM2202::stepKeysControl(double t, double dt)
+{
+    if (getKeyState(KEY_A))
+    {
+        ms_dir = 1;
+        main_shaft_timer.start();
+    }
+
+    if (getKeyState(KEY_D))
+    {
+        if (!isControl())
+        {
+            ms_dir = -1;
+            main_shaft_timer.start();
+        }
+        else
+        {
+            ms_position = MS_ZERO;
+        }
+    }
+
+    if (getKeyState(KEY_W))
+    {
+        rs_dir = 1;
+        revers_shaft_timer.start();
+    }
+
+    if (getKeyState(KEY_S))
+    {
+        rs_dir = -1;
+        revers_shaft_timer.start();
+    }
+
+    main_shaft_timer.step(t, dt);
+    revers_shaft_timer.step(t, dt);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControllerKM2202::slotRotateMainShaft()
+{
+    // Механическая блокировка поворота главного вала
+    // в нулевом положении реверсивного
+    if (rs_position != RS_ZERO)
+    {
+        ms_position += ms_dir;
+        ms_position = cut(ms_position, static_cast<int>(MS_ZERO), static_cast<int>(MS_MAX_POSITION));
+    }
+
+    main_shaft_timer.stop();
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControllerKM2202::slotRotateReversShaft()
+{
+    // Механическая блокировка реверсивки на рабочих позициях
+    if (ms_position == MS_ZERO)
+    {
+        rs_position += rs_dir;
+        rs_position = cut(rs_position, static_cast<int>(RS_BACKWARD), static_cast<int>(RS_FORWARD));
+    }
+
+    revers_shaft_timer.stop();
+}
