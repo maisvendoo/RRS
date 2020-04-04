8dd3e06afb18de01ef127c3b94ef6d8b04094b0e
maisvendoo <maisvendoo@gmail.com>
2020-04-04 15:30:54 +0300

    ТЭП70: начата реализация стартер-генератора

--- addons/tep70/tep70/include/starter-generator.h
+++ addons/tep70/tep70/include/starter-generator.h
@@ -16,7 +16,42 @@ public:
 
 private:
 
+    /// Напряжение на якоре
+    double  Ua;
 
+    /// Напряжение на обмотке возбуждения (в режиме генератора)
+    double  Uf;
+
+    /// Ток якоря
+    double  Ia;
+
+    /// Ток возбуждения
+    double  If;
+
+    /// Сопротивление обмотки якоря
+    double  Ra;
+
+    /// Сопротивление обмотки возбуждения
+    double  Rf;
+
+    /// Постоянная времени нарастания тока якоря
+    double  Ta;
+
+    /// Постоянная времени нарастания тока возбуждения
+    double  Tf;
+
+    /// Признак двигательного режима
+    bool    is_motor;
+
+    void preStep(state_vector_t &Y, double t);
+
+    void ode_system(const state_vector_t &Y, state_vector_t &dYdt, double t);
+
+    void load_config(CfgReader &cfg);
+
+    void ode_system_motor(const state_vector_t &Y, state_vector_t &dYdt, double t);
+
+    void ode_system_generator(const state_vector_t &Y, state_vector_t &dYdt, double t);
 };
 
 #endif // STARTER_GENERATOR_H
--- addons/tep70/tep70/src/starter-generator.cpp
+++ addons/tep70/tep70/src/starter-generator.cpp
@@ -4,6 +4,15 @@
 //
 //------------------------------------------------------------------------------
 StarterGenerator::StarterGenerator(QObject *parent) : Device(parent)
+  , Ua(0.0)
+  , Uf(0.0)
+  , Ia(0.0)
+  , If(0.0)
+  , Ra(0.039)
+  , Rf(6.4)
+  , Ta(0.1)
+  , Tf(0.1)
+  , is_motor(true)
 {
 
 }
@@ -14,4 +23,60 @@ StarterGenerator::StarterGenerator(QObject *parent) : Device(parent)
 StarterGenerator::~StarterGenerator()
 {
 
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void StarterGenerator::preStep(state_vector_t &Y, double t)
+{
+    Q_UNUSED(t)
+
+    Ia = Y[0];
+    If = Y[1];
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void StarterGenerator::ode_system(const state_vector_t &Y,
+                                  state_vector_t &dYdt,
+                                  double t)
+{
+    if (is_motor)
+    {
+        ode_system_motor(Y, dYdt, t);
+    }
+    else
+    {
+        ode_system_generator(Y, dYdt, t);
+    }
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void StarterGenerator::load_config(CfgReader &cfg)
+{
+
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void StarterGenerator::ode_system_motor(const state_vector_t &Y,
+                                        state_vector_t &dYdt,
+                                        double t)
+{
+    Q_UNUSED(t)
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void StarterGenerator::ode_system_generator(const state_vector_t &Y,
+                                            state_vector_t &dYdt,
+                                            double t)
+{
+    Q_UNUSED(t)
 }
