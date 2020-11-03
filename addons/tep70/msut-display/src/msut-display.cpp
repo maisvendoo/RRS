1dd6f0905bd224c88a41c99bc0581e54bf52ce7d
bogos <bogos.dagldian@yandex.ru>
2020-11-03 17:39:32 +0300

    правка родителя

--- addons/tep70/msut-display/src/msut-display.cpp
+++ addons/tep70/msut-display/src/msut-display.cpp
@@ -98,58 +98,58 @@ void MsutDisplay::init()
 
     // ВУ1
     //
-    frameVU1_Ited_ = new QFrame(this);
+    frameVU1_Ited_ = new QFrame(background);
     frameVU1_Ited_->setAutoFillBackground(true);
     frameVU1_Ited_->setPalette(QPalette(Qt::blue));
     frameVU1_Ited_->resize(13, 230);
     frameVU1_Ited_->move(179, 68);
 
-    labelVU1_Ited_ = new QLabel(this);
+    labelVU1_Ited_ = new QLabel(background);
     drawNumberLabel_(labelVU1_Ited_, QRect(150,314, 50,20), 14, "blue");
     labelVU1_Ited_->setText("0");
 
     //
-    frameVU1_I_ = new QFrame(this);
+    frameVU1_I_ = new QFrame(background);
     frameVU1_I_->setAutoFillBackground(true);
     frameVU1_I_->setPalette(QPalette(Qt::red));
     frameVU1_I_->resize(13, 230);
     frameVU1_I_->move(229, 68);
 
-    labelVU1_I_ = new QLabel(this);
+    labelVU1_I_ = new QLabel(background);
     drawNumberLabel_(labelVU1_I_, QRect(204,314, 50,20), 14, "red");
     labelVU1_I_->setText("0");
 
     //
-    frameVU1_U_ = new QFrame(this);
+    frameVU1_U_ = new QFrame(background);
     frameVU1_U_->setAutoFillBackground(true);
     frameVU1_U_->setPalette(QPalette(Qt::blue));
     frameVU1_U_->resize(13, 230);
     frameVU1_U_->move(280, 68);
 
-    labelVU1_U_ = new QLabel(this);
+    labelVU1_U_ = new QLabel(background);
     drawNumberLabel_(labelVU1_U_, QRect(258,314, 50,20), 14, "blue");
     labelVU1_U_->setText("0");
 
     // ВУ1
     //
-    frameVU2_U_ = new QFrame(this);
+    frameVU2_U_ = new QFrame(background);
     frameVU2_U_->setAutoFillBackground(true);
     frameVU2_U_->setPalette(QPalette(Qt::blue));
     frameVU2_U_->resize(13, 230);
     frameVU2_U_->move(540, 68);
 
-    labelVU2_U_ = new QLabel(this);
+    labelVU2_U_ = new QLabel(background);
     drawNumberLabel_(labelVU2_U_, QRect(530,314, 50,20), 14, "blue");
     labelVU2_U_->setText("0");
 
     //
-    frameVU2_I_ = new QFrame(this);
+    frameVU2_I_ = new QFrame(background);
     frameVU2_I_->setAutoFillBackground(true);
     frameVU2_I_->setPalette(QPalette(Qt::red));
     frameVU2_I_->resize(13, 230);
     frameVU2_I_->move(596, 68);
 
-    labelVU2_I_ = new QLabel(this);
+    labelVU2_I_ = new QLabel(background);
     drawNumberLabel_(labelVU2_I_, QRect(592,314, 50,20), 14, "red");
     labelVU2_I_->setText("0");
 
@@ -158,12 +158,12 @@ void MsutDisplay::init()
 
     // kW
     //
-    label_kW_left_ = new QLabel(this);
+    label_kW_left_ = new QLabel(background);
     drawNumberLabel_(label_kW_left_, QRect(327,256, 50,20), 14, "blue", Qt::AlignLeft);
     label_kW_left_->setText("0");
 
     //
-    label_kW_right_ = new QLabel(this);
+    label_kW_right_ = new QLabel(background);
     drawNumberLabel_(label_kW_right_, QRect(456,256, 50,20), 14, "red", Qt::AlignRight);
     label_kW_right_->setText("0");
 
@@ -171,12 +171,12 @@ void MsutDisplay::init()
     // РЕВЕРСОР
 
     // ПОЗИЦИЯ
-    labelPositin_ = new QLabel(this);
+    labelPositin_ = new QLabel(background);
     drawNumberLabel_(labelPositin_, QRect(35,146, 80,48), 35, "white");
     labelPositin_->setText("0");
 
     // РЕЖИМ
-    labelRezim_ = new QLabel(this);
+    labelRezim_ = new QLabel(background);
     drawNumberLabel_(labelRezim_, QRect(15,244, 115,45), 14, "white");
     labelRezim_->setText("СТОП");
 
