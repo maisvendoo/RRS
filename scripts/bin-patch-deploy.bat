4fe265cff73f92248c2fea5d81e7a088db424141
Mikhail Arepev <arepmm@gmail.com>
2024-06-09 18:29:34 +0300

    Обновил скрипт развёртывания бинарных файлов, SDK и конфигов

--- scripts/bin-patch-deploy.bat
+++ scripts/bin-patch-deploy.bat
@@ -1,45 +1,26 @@
-rem Создаем структуру каталогов игры
-
-mkdir %RRS_DEV_ROOT%
-mkdir %RRS_DEV_ROOT%\bin
-mkdir %RRS_DEV_ROOT%\lib
-mkdir %RRS_DEV_ROOT%\modules
-mkdir %RRS_DEV_ROOT%\plugins
-
-mkdir %RRS_DEV_ROOT%\cfg
-mkdir %RRS_DEV_ROOT%\data
-mkdir %RRS_DEV_ROOT%\routes
-mkdir %RRS_DEV_ROOT%\fonts
-mkdir %RRS_DEV_ROOT%\themes
-
-mkdir %RRS_DEV_ROOT%\sdk
-mkdir %RRS_DEV_ROOT%\sdk\include
-
 rem Копируем бинарные файлы
 
-xcopy  ..\..\bin\*.exe %RRS_DEV_ROOT%\bin\
-xcopy ..\..\lib\*.dll %RRS_DEV_ROOT%\bin\
+xcopy /Y  ..\..\bin\*.exe %RRS_DEV_ROOT%\bin\
+xcopy /Y ..\..\lib\*.dll %RRS_DEV_ROOT%\bin\
 move %RRS_DEV_ROOT%\bin\rkf5.dll %RRS_DEV_ROOT%\lib\rkf5.dll
 move %RRS_DEV_ROOT%\bin\rk4.dll %RRS_DEV_ROOT%\lib\rk4.dll
 move %RRS_DEV_ROOT%\bin\euler2.dll %RRS_DEV_ROOT%\lib\euler2.dll
 move %RRS_DEV_ROOT%\bin\euler.dll %RRS_DEV_ROOT%\lib\euler.dll
 
-xcopy ..\..\modules\*.dll %RRS_DEV_ROOT%\modules
-xcopy ..\..\modules\chs2t\*.dll %RRS_DEV_ROOT%\modules\chs2t\
-xcopy ..\..\modules\freightcar\*.dll %RRS_DEV_ROOT%\modules\freightcar\
-xcopy ..\..\modules\passcar\*.dll %RRS_DEV_ROOT%\modules\passcar\
-xcopy ..\..\modules\tep70\*.dll %RRS_DEV_ROOT%\modules\tep70\
-xcopy ..\..\modules\vl60k\*.dll %RRS_DEV_ROOT%\modules\vl60k\
-xcopy ..\..\modules\vl60pk\*.dll %RRS_DEV_ROOT%\modules\vl60pk\
+xcopy /Y  ..\..\modules\*.dll %RRS_DEV_ROOT%\modules
+xcopy /Y  ..\..\modules\freightcar\*.dll %RRS_DEV_ROOT%\modules\freightcar\
+xcopy /Y  ..\..\modules\passcar\*.dll %RRS_DEV_ROOT%\modules\passcar\
+xcopy /Y  ..\..\modules\vl60k\*.dll %RRS_DEV_ROOT%\modules\vl60k\
+xcopy /Y  ..\..\modules\vl60pk\*.dll %RRS_DEV_ROOT%\modules\vl60pk\
 
-xcopy ..\..\plugins\*.dll %RRS_DEV_ROOT%\plugins
+xcopy /Y  ..\..\plugins\*.dll %RRS_DEV_ROOT%\plugins
 
 rem Копируем SDK
 
-xcopy /Q ..\CfgReader\include\*.h %RRS_DEV_ROOT%\sdk\include\
-xcopy /Q ..\filesystem\include\*.h %RRS_DEV_ROOT%\sdk\include\
-xcopy /Q ..\simulator\solver\include\*.h %RRS_DEV_ROOT%\sdk\include\
-xcopy /Q ..\simulator\physics\include\*.h %RRS_DEV_ROOT%\sdk\include\
-xcopy /Q ..\simulator\vehicle\include\*.h %RRS_DEV_ROOT%\sdk\include\
-xcopy /Q ..\simulator\device\include\*.h %RRS_DEV_ROOT%\sdk\include\
-xcopy /Q ..\viewer\display\include\*.h %RRS_DEV_ROOT%\sdk\include\
+xcopy /Q /Y  ..\CfgReader\include\*.h %RRS_DEV_ROOT%\sdk\include\
+xcopy /Q /Y  ..\filesystem\include\*.h %RRS_DEV_ROOT%\sdk\include\
+xcopy /Q /Y  ..\simulator\solver\include\*.h %RRS_DEV_ROOT%\sdk\include\
+xcopy /Q /Y  ..\simulator\physics\include\*.h %RRS_DEV_ROOT%\sdk\include\
+xcopy /Q /Y  ..\simulator\vehicle\include\*.h %RRS_DEV_ROOT%\sdk\include\
+xcopy /Q /Y  ..\simulator\device\include\*.h %RRS_DEV_ROOT%\sdk\include\
+xcopy /Q /Y  ..\viewer\display\include\*.h %RRS_DEV_ROOT%\sdk\include\
--- scripts/patch-deploy.bat
+++ scripts/patch-deploy.bat
@@ -1,59 +1,42 @@
-rem Создаем структуру каталогов игры
-
-mkdir %RRS_DEV_ROOT%
-mkdir %RRS_DEV_ROOT%\bin
-mkdir %RRS_DEV_ROOT%\lib
-mkdir %RRS_DEV_ROOT%\modules
-mkdir %RRS_DEV_ROOT%\plugins
-
-mkdir %RRS_DEV_ROOT%\cfg
-mkdir %RRS_DEV_ROOT%\data
-mkdir %RRS_DEV_ROOT%\routes
-mkdir %RRS_DEV_ROOT%\fonts
-mkdir %RRS_DEV_ROOT%\themes
-
-mkdir %RRS_DEV_ROOT%\sdk
-mkdir %RRS_DEV_ROOT%\sdk\include
-
 rem Копируем бинарные файлы
 
-xcopy  ..\..\bin\*.exe %RRS_DEV_ROOT%\bin\
-xcopy ..\..\lib\*.dll %RRS_DEV_ROOT%\bin\
+xcopy /Y  ..\..\bin\*.exe %RRS_DEV_ROOT%\bin\
+xcopy /Y ..\..\lib\*.dll %RRS_DEV_ROOT%\bin\
 move %RRS_DEV_ROOT%\bin\rkf5.dll %RRS_DEV_ROOT%\lib\rkf5.dll
 move %RRS_DEV_ROOT%\bin\rk4.dll %RRS_DEV_ROOT%\lib\rk4.dll
 move %RRS_DEV_ROOT%\bin\euler2.dll %RRS_DEV_ROOT%\lib\euler2.dll
 move %RRS_DEV_ROOT%\bin\euler.dll %RRS_DEV_ROOT%\lib\euler.dll
 
-xcopy ..\..\modules\*.dll %RRS_DEV_ROOT%\modules
-xcopy ..\..\modules\vl60\*.dll %RRS_DEV_ROOT%\modules\vl60\
-xcopy ..\..\modules\passcar\*.dll %RRS_DEV_ROOT%\modules\passcar\
-xcopy ..\..\modules\freightcar\*.dll %RRS_DEV_ROOT%\modules\freightcar\
+xcopy /Y  ..\..\modules\*.dll %RRS_DEV_ROOT%\modules
+xcopy /Y  ..\..\modules\freightcar\*.dll %RRS_DEV_ROOT%\modules\freightcar\
+xcopy /Y  ..\..\modules\passcar\*.dll %RRS_DEV_ROOT%\modules\passcar\
+xcopy /Y  ..\..\modules\vl60k\*.dll %RRS_DEV_ROOT%\modules\vl60k\
+xcopy /Y  ..\..\modules\vl60pk\*.dll %RRS_DEV_ROOT%\modules\vl60pk\
 
-xcopy ..\..\plugins\*.dll %RRS_DEV_ROOT%\plugins
+xcopy /Y  ..\..\plugins\*.dll %RRS_DEV_ROOT%\plugins
 
 rem Копируем конфиги
 
-xcopy ..\cfg\*.xml %RRS_DEV_ROOT%\cfg\
+xcopy /Y ..\cfg\*.xml %RRS_DEV_ROOT%\cfg\
 del %RRS_DEV_ROOT%\cfg\control-panel.xml
-xcopy ..\cfg\couplings\*.xml %RRS_DEV_ROOT%\cfg\couplings\
-xcopy ..\cfg\devices\*.xml %RRS_DEV_ROOT%\cfg\devices\
+xcopy /Y ..\cfg\devices\*.xml %RRS_DEV_ROOT%\cfg\devices\
+xcopy /Y ..\cfg\main-resist\*.xml %RRS_DEV_ROOT%\cfg\main-resist\
+xcopy /Y ..\cfg\wheel-rail-friction\*.xml %RRS_DEV_ROOT%\cfg\wheel-rail-friction\
 
-xcopy ..\cfg\main-resist\default.xml %RRS_DEV_ROOT%\cfg\main-resist\
-xcopy ..\cfg\main-resist\passcar.xml %RRS_DEV_ROOT%\cfg\main-resist\
-xcopy ..\cfg\main-resist\loco-resist.xml %RRS_DEV_ROOT%\cfg\main-resist\
+xcopy /Y ..\cfg\vehicles\vl60pk-1543\*.* %RRS_DEV_ROOT%\cfg\vehicles\vl60pk-1543\
+xcopy /Y ..\cfg\vehicles\VL60k-1737\*.* %RRS_DEV_ROOT%\cfg\vehicles\VL60k-1737\
+xcopy /Y ..\cfg\vehicles\IMR_pass_rzd-11100\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-11100\
+xcopy /Y ..\cfg\vehicles\IMR_pass_rzd-13819\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-13819\
+xcopy /Y ..\cfg\vehicles\IMR_pass_rzd-16733\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-16733\
+xcopy /Y ..\cfg\vehicles\IMR_pass_rzd-17669\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-17669\
+xcopy /Y ..\cfg\vehicles\IMR_pass_rzd-21071\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-21071\
+xcopy /Y ..\cfg\vehicles\IMR_pass_rzd-25924\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-25924\
+xcopy /Y ..\cfg\vehicles\IMR_pass_rzd-65361\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-65361\
+xcopy /Y ..\cfg\vehicles\Fr_hopper_RZD-2851\*.* %RRS_DEV_ROOT%\cfg\vehicles\Fr_hopper_RZD-2851\
 
-xcopy ..\cfg\wheel-rail-friction\*.xml %RRS_DEV_ROOT%\cfg\wheel-rail-friction\
-
-xcopy ..\cfg\vehicles\vl60pk-1543\*.* %RRS_DEV_ROOT%\cfg\vehicles\vl60pk-1543\
-xcopy ..\cfg\vehicles\IMR_pass_rzd-11100\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-11100\
-xcopy ..\cfg\vehicles\IMR_pass_rzd-13819\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-13819\
-xcopy ..\cfg\vehicles\IMR_pass_rzd-16733\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-16733\
-xcopy ..\cfg\vehicles\IMR_pass_rzd-17669\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-17669\
-xcopy ..\cfg\vehicles\IMR_pass_rzd-25924\*.* %RRS_DEV_ROOT%\cfg\vehicles\IMR_pass_rzd-25924\
-
-xcopy ..\cfg\trains\vl60pk-1543.xml %RRS_DEV_ROOT%\cfg\trains\
-xcopy ..\cfg\trains\vl60pk-1543-pass-train.xml %RRS_DEV_ROOT%\cfg\trains\
-xcopy ..\cfg\trains\VL60k-1737.xml %RRS_DEV_ROOT%\cfg\trains\
-xcopy ..\cfg\trains\vl60k-1737-frEmpties.xml %RRS_DEV_ROOT%\cfg\trains\
-xcopy ..\cfg\trains\vl60k-1737-frLoads.xml %RRS_DEV_ROOT%\cfg\trains\
+xcopy /Y ..\cfg\trains\vl60pk-1543.xml %RRS_DEV_ROOT%\cfg\trains\
+xcopy /Y ..\cfg\trains\vl60pk-1543-T65_17.xml %RRS_DEV_ROOT%\cfg\trains\
+xcopy /Y ..\cfg\trains\VL60k-1737.xml %RRS_DEV_ROOT%\cfg\trains\
+xcopy /Y ..\cfg\trains\vl60k-1737-frEmpties.xml %RRS_DEV_ROOT%\cfg\trains\
+xcopy /Y ..\cfg\trains\vl60k-1737-frLoads.xml %RRS_DEV_ROOT%\cfg\trains\
 
