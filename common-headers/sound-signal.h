8a16cc750a85f722cd2a0cb34b690dbbb989d32b
Mikhail Arepev <arepmm@gmail.com>
2024-07-14 10:12:36 +0300

    Добавлены комментарии к коду

--- common-headers/sound-signal.h
+++ common-headers/sound-signal.h
@@ -4,13 +4,16 @@
 #include    <cmath>
 
 //------------------------------------------------------------------------------
-//
+// Состояние звука и создание-расшифровка float-сигнала управления звуком
+// В float-сигнале: знак - логический сигнал включить-выключить
+//                  целая часть - скорость воспроизведения в процентах, 50 - 200
+//                  дробная часть - громкость звука в долях единицы, 0.0 - 0.999
 //------------------------------------------------------------------------------
 struct sound_state_t
 {
-    bool  play;
-    float volume;
-    float pitch;
+    bool  play;     ///< Сигнал включить-выключить звук
+    float volume;   ///< Громкость звука, 0.0 - 1.0
+    float pitch;    ///< Скорость воспроизведения звука, 0.5-2.0
 
     sound_state_t(bool play = false, float volume = 0.999f, float pitch = 1.0f)
         : play(play)
@@ -20,6 +23,7 @@ struct sound_state_t
 
     }
 
+    // Сигнал включить-выключить звук, с максимальной громкостью и обычной скоростью
     static float createSoundSignal(bool play)
     {
         if (play)
@@ -28,21 +32,29 @@ struct sound_state_t
             return -100.999f;
     }
 
-    static float createSoundSignal(bool play, float volume = 0.999f, float pitch = 1.0f)
+    // Сигнал управления звуком
+    static float createSoundSignal(bool play, float volume, float pitch = 1.0f)
     {
+        // Проверяем предельные значения громкости 0.0 - 0.999
         float v = std::fmax( 0.0f, std::fmin(0.999f, volume) );
-        float p = std::fmax( 0.4f, std::fmin(2.0f, pitch) );
+
+        // Проверяем предельные значения скорости, 0.5 - 2.0
+        // Если меньше 0.5, звук отключится сигналом скорости 49%
+        float p = std::fmax( 0.49f, std::fmin(2.0f, pitch) );
+
         if (play)
             return floorf(100.0f * p) + v;
         else
             return -(floorf(100.0f * p) + v);
     }
 
+    // Сигнал состояния звука
     float createSoundSignal()
     {
         return createSoundSignal(play, volume, pitch);
     }
 
+    // Расшифровка сигнала в состояние звука
     sound_state_t soundFromSignal(const float signal)
     {
         float tmp_signal = signal;
