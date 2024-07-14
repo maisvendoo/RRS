3ac854f3cb517a4f8adece1421cf9cb4e2d0b98c
Mikhail Arepev <arepmm@gmail.com>
2024-07-15 01:12:51 +0300

    Revert "Вернул временно read_custom_config"
    This reverts commit af314ffecd97653f7f18dabbffca5f3bf269cb78.

--- simulator/device/include/device.h
+++ simulator/device/include/device.h
@@ -87,9 +87,6 @@ public:
     /// Read device config file
     virtual void read_config(const QString &filename, const QString &dir_path = "");
 
-    /// Временно вернул, для обратной совместимости
-    virtual void read_custom_config(const QString &path);
-
     QString getDebugMsg() const;
 
     ///
