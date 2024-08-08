877b61a675a4e923297b217cf241fdc2e6afb7a9
maisvendoo <maisvendoo@gmail.com>
2024-08-08 15:59:17 +0300

    Реализована структура описывающая трэк - прямолинейный отрезок траектории

--- simulator/topology/include/track.h
+++ simulator/topology/include/track.h
@@ -6,25 +6,44 @@
 //------------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------------
-class Track
+struct track_t
 {
-public:
+    dvec3 begin_point = {0.0, 0.0, 0.0};
 
-    Track() = default;
+    dvec3 end_point = {0.0, 0.0, 0.0};
 
-    ~Track() = default;
+    dvec3 orth = {0.0, 0.0, 0.0};
 
-private:
+    dvec3 trav = {0.0, 0.0, 0.0};
 
-    dvec3 begin_point;
+    dvec3 up = {0.0, 0.0, 0.0};
 
-    dvec3 end_point;
+    double len = 0.0;
 
-    dvec3 orth;
+    track_t()
+    {
 
-    dvec3 trav;
+    }
 
-    dvec3 up;
+    track_t(dvec3 p0, dvec3 p1)
+    {
+        begin_point = p0;
+        end_point = p1;
+
+        dvec3 t = end_point - begin_point;
+        len = length(t);
+
+        orth = normalize(t);
+
+        trav.z = 0.0;
+        trav.x = t.y;
+        trav.y = t.x;
+
+        trav = normalize(trav);
+
+        up = cross(trav, t);
+        up = normalize(up);
+    }
 };
 
 #endif
