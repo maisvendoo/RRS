2562d113519581b14045ed769f7e1dba81684388
maisvendoo <maisvendoo@gmail.com>
2024-09-18 13:52:11 +0300

    Сериализация перечня сигналов, имеющихся в топологии

--- simulator/topology/include/signals-data-types.h
+++ simulator/topology/include/signals-data-types.h
@@ -9,6 +9,36 @@
 struct signals_data_t
 {
     std::vector<Signal *> line_signals;
+
+    QByteArray serialize()
+    {
+        QByteArray tmp_data;
+        QBuffer buff(&tmp_data);
+        buff.open(QIODevice::WriteOnly);
+        QDataStream stream(&buff);
+
+        for (auto line_signal : line_signals)
+        {
+            stream << line_signal->serialize();
+        }
+
+        return buff.data();
+    }
+
+    void deserialize(QByteArray &data)
+    {
+        QBuffer buff(&data);
+        buff.open(QIODevice::ReadOnly);
+        QDataStream stream(&buff);
+
+        for (auto line_signal : line_signals)
+        {
+            QByteArray tmp_data;
+            stream >> tmp_data;
+
+            line_signal->deserialize(tmp_data);
+        }
+    }
 };
 
 #endif
