d04ec010b73b52db38617943f492d8ce7689e9cd
maisvendoo <maisvendoo@yandex.ru>
2024-09-22 17:59:15 +0300

    Начата реализация выходных сигналов

--- simulator/topology/include/exit-signal.h
+++ simulator/topology/include/exit-signal.h
@@ -2,6 +2,8 @@
 #define     EXIT_SIGNAL_H
 
 #include    <rail-signal.h>
+#include    <combine-releay.h>
+#include    <timer.h>
 
 //------------------------------------------------------------------------------
 //
@@ -16,13 +18,44 @@ public:
 
     void step(double t, double dt) override;
 
+public slots:
+
+    void slotPressOpen();
+
+    void slotPressClose();
+
 private:
+
+    enum
+    {
+        NUM_SR_CONTACTS = 3,
+        SR_SELF = 0,
+        SR_DLR_CTRL = 1,
+        SR_SRS_CTRL = 2
+    };
+
+    /// Сигнальное реле
+    Relay *signal_relay = new Relay(NUM_SR_CONTACTS);
+
+    bool is_open_button_pressed = false;
+
+    bool is_close_button_unpressed = true;
+
+    Timer *open_timer = new Timer(1.0, false);
+
+    Timer *close_timer = new Timer(1.0, false);
 
     void preStep(state_vector_t &Y, double t) override;
 
     void ode_system(const state_vector_t &Y,
                     state_vector_t &dYdt,
                     double t) override;
+
+private slots:
+
+    void slotOpenTimer();
+
+    void slotCloseTimer();
 };
 
 #endif
--- simulator/topology/src/exit-signal.cpp
+++ simulator/topology/src/exit-signal.cpp
@@ -5,7 +5,13 @@
 //------------------------------------------------------------------------------
 ExitSignal::ExitSignal(QObject *parent) : Signal(parent)
 {
+    signal_relay->read_config("combine-relay");
+    signal_relay->setInitContactState(SR_SELF, false);
+    signal_relay->setInitContactState(SR_DLR_CTRL, false);
+    signal_relay->setInitContactState(SR_SRS_CTRL, false);
 
+    connect(open_timer, &Timer::process, this, &ExitSignal::slotOpenTimer);
+    connect(close_timer, &Timer::process, this, &ExitSignal::slotCloseTimer);
 }
 
 //------------------------------------------------------------------------------
@@ -22,6 +28,29 @@ ExitSignal::~ExitSignal()
 void ExitSignal::step(double t, double dt)
 {
     Signal::step(t, dt);
+
+    signal_relay->step(t, dt);
+
+    open_timer->step(t, dt);
+    close_timer->step(t, dt);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ExitSignal::slotPressOpen()
+{
+    is_open_button_pressed = true;
+    open_timer->start();
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ExitSignal::slotPressClose()
+{
+    is_close_button_unpressed = false;
+    close_timer->start();
 }
 
 //------------------------------------------------------------------------------
@@ -40,4 +69,22 @@ void ExitSignal::ode_system(const state_vector_t &Y,
                             double t)
 {
 
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ExitSignal::slotOpenTimer()
+{
+    is_open_button_pressed = false;
+    open_timer->stop();
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ExitSignal::slotCloseTimer()
+{
+    is_close_button_unpressed = true;
+    close_timer->stop();
 }
