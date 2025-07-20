75664e8f782e5ba11e6d2af9747ada159994a258
Mikhail Arepev <arepmm@gmail.com>
2025-07-20 19:30:46 +0300

    Структура для времени суток на сервере симулятора

--- common-headers/datetime.h
+++ common-headers/datetime.h
@@ -3,6 +3,14 @@
 
 #include    <cstdint>
 
+// Храним время суток в 10-тысячных долях секунды от полуночи
+#define TIMEUNIT_MULTIPLIER         10000
+#define TIMEUNIT_MULTIPLIER_DAY     TIMEUNIT_MULTIPLIER * 60 * 60 * 24
+#define TIMEUNIT_MULTIPLIER_HOUR    TIMEUNIT_MULTIPLIER * 60 * 60
+#define TIMEUNIT_MULTIPLIER_MIN     TIMEUNIT_MULTIPLIER * 60
+#define TIMEUNIT_MULTIPLIER_SEC     TIMEUNIT_MULTIPLIER
+#define TIMEUNIT_MULTIPLIER_MSEC    TIMEUNIT_MULTIPLIER / 1000
+
 //------------------------------------------------------------------------------
 // Структура для хранения даты сервера RRS
 //------------------------------------------------------------------------------
@@ -115,6 +123,65 @@ public:
         }
         ++d;
         return;
+    }
+};
+
+//------------------------------------------------------------------------------
+// Структура для хранения времени суток сервера RRS
+//------------------------------------------------------------------------------
+struct server_time_t final
+{
+private:
+    uint32_t time_unit_since_midnight;
+
+public:
+    server_time_t()
+        : time_unit_since_midnight(0)
+    {
+    }
+
+    server_time_t(uint8_t hour, uint8_t minute, uint8_t sec, uint16_t msec = 0)
+    {
+        time_unit_since_midnight = TIMEUNIT_MULTIPLIER_MSEC * ((msec < 1000) ? msec : 999);
+        time_unit_since_midnight += TIMEUNIT_MULTIPLIER_SEC * ((sec < 60) ? sec : 59);
+        time_unit_since_midnight += TIMEUNIT_MULTIPLIER_MIN * ((minute < 60) ? minute : 59);
+        time_unit_since_midnight += TIMEUNIT_MULTIPLIER_HOUR * ((hour < 24) ? hour : 23);
+    }
+
+    uint8_t hour() const
+    {
+        return time_unit_since_midnight / TIMEUNIT_MULTIPLIER_HOUR;
+    }
+
+    uint8_t minute() const
+    {
+        return time_unit_since_midnight / TIMEUNIT_MULTIPLIER_MIN % 60;
+    }
+
+    uint8_t sec() const
+    {
+        return time_unit_since_midnight / TIMEUNIT_MULTIPLIER_SEC % 60;
+    }
+
+    uint16_t msec() const
+    {
+        return time_unit_since_midnight / TIMEUNIT_MULTIPLIER_MSEC % 1000;
+    }
+
+    /// Интегрирование времени, возвращает true если нужен переход к следующему дню
+    bool addTime(double add_sec)
+    {
+        if (add_sec < 0.0)
+            return false;
+
+        uint32_t add_timeunit = add_sec * TIMEUNIT_MULTIPLIER_SEC;
+        time_unit_since_midnight += add_timeunit;
+
+        if (time_unit_since_midnight < TIMEUNIT_MULTIPLIER_DAY)
+            return false;
+
+        time_unit_since_midnight = time_unit_since_midnight - TIMEUNIT_MULTIPLIER_DAY;
+        return true;
     }
 };
 
