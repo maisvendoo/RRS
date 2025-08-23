d9819d901282c417dabccfe47467f313d4a248c1
Mikhail Arepev <arepmm@gmail.com>
2025-08-23 17:21:16 +0300

    Поправил папку назначения в развёртывании анимаций

--- scripts/windeploy.bat
+++ scripts/windeploy.bat
@@ -76,13 +76,11 @@ xcopy /Y /S %DATA_PATH%\shaders\*.* %RRS_DEV_ROOT%\data\shaders\
 
 rem Копируем данные игры (модели, звуки, конфиги анимаций, шрифты)
 
-rem ВЛ60пк
+rem ВЛ60пк и ВЛ60к
 xcopy /Y /S %DATA_PATH%\models\VL60pk-1543\*.* %RRS_DEV_ROOT%\data\models\VL60pk-1543\
-xcopy /Y /S %DATA_PATH%\sounds\vl60\*.* %RRS_DEV_ROOT%\data\sounds\vl60\
-xcopy /Y /S %DATA_PATH%\animations\vl60\*.* %RRS_DEV_ROOT%\data\animations\vl60pk\
-
-rem ВЛ60к
 xcopy /Y /S %DATA_PATH%\models\VL60k-1737\*.* %RRS_DEV_ROOT%\data\models\VL60k-1737\
+xcopy /Y /S %DATA_PATH%\sounds\vl60\*.* %RRS_DEV_ROOT%\data\sounds\vl60\
+xcopy /Y /S %DATA_PATH%\animations\vl60\*.* %RRS_DEV_ROOT%\data\animations\vl60\
 
 rem Пассажирские вагоны
 xcopy /Y /S %DATA_PATH%\models\IMR_pass_rzd\*.* %RRS_DEV_ROOT%\data\models\IMR_pass_rzd\
