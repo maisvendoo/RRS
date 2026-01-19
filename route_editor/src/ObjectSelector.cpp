2bba03c9b9d7772751a13b8b49e21dde0132102d
avilkinnick <avilkin.nick@gmail.com>
2026-01-19 08:02:04 +0300

    Делаю ObjectSelector

--- route_editor/src/ObjectSelector.cpp
+++ route_editor/src/ObjectSelector.cpp
@@ -5,9 +5,29 @@
 #include "Outline.h"
 #include "Route.h"
 
+#include <vsg/nodes/MatrixTransform.h>
+#include <vsg/nodes/Node.h>
 #include <vsg/ui/PointerEvent.h>
+#include <vsg/utils/LineSegmentIntersector.h>
 
+#include <algorithm>
 #include <cassert>
+
+// Если нажимаем на экран, возможны следующие ситуации:
+// 1) Ничего не выделено
+//    -> Выделения и соответствующего GUI нет
+// 2) Ничего не было выделено, нажали на объект
+//    -> Объект нужно выделить и показать GUI
+// 3) Выделен объект, нажимаем на него же
+//    -> Выделение снимается, GUI убирается
+// 4) Выделен объект, нажимаем на другой объект
+//    -> Выделяется другой объект
+// 5) Выделен объект, нажимаем на пустоту
+//    -> Выделение снимается, GUI убирается
+// 6) Выделен объект, нажимаем на Gizmo
+//    -> Управляем объектом через Gizmo, пока не отпустим ЛКМ
+
+// TODO: Multiple object selection
 
 ObjectSelector::ObjectSelector(
     const settings_t& settings,
@@ -37,6 +57,56 @@ ObjectSelector::~ObjectSelector() = default;
 void ObjectSelector::apply(vsg::ButtonPressEvent& buttonPress)
 {
     if (buttonPress.handled)
+    {
+        return;
+    }
+
+    const auto lmb_intersector = intersection_handler->get_lmb_intersector();
+    if (!lmb_intersector)
+    {
+        return;
+    }
+
+    route->accept(*lmb_intersector);
+
+    auto& intersections = lmb_intersector->intersections;
+    if (intersections.empty())
+    {
+        return;
+    }
+
+    std::sort(intersections.begin(), intersections.end(),
+        [](const auto& lhs, const auto& rhs) -> bool
+        {
+            return (lhs->ratio) < (rhs->ratio);
+        }
+    );
+
+    const auto intersection = intersections.front();
+
+    const auto& node_path = intersection->nodePath;
+    if (node_path.empty())
+    {
+        return;
+    }
+
+    for (const vsg::Node* const node : node_path)
+    {
+        const auto matrix_transform = vsg::ref_ptr(
+            const_cast<vsg::MatrixTransform*>(
+                node->cast<vsg::MatrixTransform>()));
+
+        if (!matrix_transform)
+        {
+            continue;
+        }
+
+        object = matrix_transform;
+
+        break;
+    }
+
+    if (!object)
     {
         return;
     }
