9d719f7f6cd07bdfa8a0e5a102eb6c3829a009e8
maisvendoo <maisvendoo@gmail.com>
2026-01-23 16:52:30 +0300

    Структура, описывающая временной триггер

---
+++ simulator/scenario-manager/include/date-time-trigger.h
@@ -0,0 +1,23 @@
+#ifndef     DATE_TIME_TRIGGER_H
+#define     DATE_TIME_TRIGGER_H
+
+#include    <sol/sol.hpp>
+#include    <datetime.h>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+struct date_time_tirgger_t
+{
+    /// Момент времени активации триггера (с учетом даты!)
+    simulator_time_t action_time;
+    /// Функция Lua, исполняемая триггером
+    sol::protected_function atction_func;
+
+    date_time_tirgger_t()
+    {
+
+    }
+};
+
+#endif
