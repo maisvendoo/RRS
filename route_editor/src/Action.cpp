4624255e6ffdb877f0b8a3ccdd977b18e2da6a96
avilkinnick <avilkin.nick@gmail.com>
2026-02-13 15:27:57 +0300

    Добавил кнопок на будущее и упростил Action

--- route_editor/src/Action.cpp
+++ route_editor/src/Action.cpp
@@ -1,41 +1,22 @@
 #include "Action.h"
 
-#include <cassert>
+#include <map>
+
+static const std::map<Action, const char*> action_map = {
+    {ACTION_MOVE_CAMERA_FORWARD, "Camera: move forward"},
+    {ACTION_MOVE_CAMERA_BACKWARD, "Camera: move backward"},
+    {ACTION_MOVE_CAMERA_LEFT, "Camera: move left"},
+    {ACTION_MOVE_CAMERA_RIGHT, "Camera: move right"},
+    {ACTION_MOVE_OBJECTS, "Objects: Grab"},
+    {ACTION_ROTATE_OBJECTS, "Objects: Rotate"},
+    {ACTION_SCALE_OBJECTS, "Objects: Scale"},
+    {ACTION_COPY_OBJECTS, "Objects: Copy"},
+    {ACTION_PASTE_OBJECTS, "Objects: Paste"},
+    {ACTION_UNDO_COMMAND, "Undo command"},
+    {ACTION_REDO_COMMAND, "Redo command"}
+};
 
 const char* to_c_string(Action action)
 {
-    assert(action >= 0);
-    assert(action < TOTAL_ACTIONS);
-
-    switch (action)
-    {
-        case ACTION_MOVE_CAMERA_FORWARD:
-        {
-            return "Camera: move forward";
-        }
-        case ACTION_MOVE_CAMERA_BACKWARD:
-        {
-            return "Camera: move backward";
-        }
-        case ACTION_MOVE_CAMERA_LEFT:
-        {
-            return "Camera: move left";
-        }
-        case ACTION_MOVE_CAMERA_RIGHT:
-        {
-            return "Camera: move right";
-        }
-        case ACTION_MOVE_OBJECTS:
-        {
-            return "Object: Grab";
-        }
-        case ACTION_ROTATE_OBJECTS:
-        {
-            return "Object: Rotate";
-        }
-        default:
-        {
-            return "";
-        }
-    }
+    return action_map.at(action);
 }
