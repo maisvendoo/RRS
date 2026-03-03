683b4926661a8a17652384db59302f15bf4ff5c3
avilkinnick <avilkin.nick@gmail.com>
2026-03-03 07:27:08 +0300

    Убрал map из Action

--- route_editor/src/Action.cpp
+++ route_editor/src/Action.cpp
@@ -1,36 +1,29 @@
 #include "Action.h"
 
-#include <iterator>
-#include <map>
+static const char* const * get_action_names()
+{
+    static const char* action_names[TOTAL_ACTIONS];
 
-using ActionNameMap = std::map<Action, const char*>;
-using ActionNamePair = ActionNameMap::value_type;
+    action_names[ACTION_MOVE_CAMERA_FORWARD] = "Camera: move forward";
+    action_names[ACTION_MOVE_CAMERA_BACKWARD] = "Camera: move backward";
+    action_names[ACTION_MOVE_CAMERA_LEFT] = "Camera: move left";
+    action_names[ACTION_MOVE_CAMERA_RIGHT] = "Camera: move right";
+    action_names[ACTION_MOVE_OBJECTS] = "Objects: Grab";
+    action_names[ACTION_ROTATE_OBJECTS] = "Objects: Rotate";
+    action_names[ACTION_SCALE_OBJECTS] = "Objects: Scale";
+    action_names[ACTION_COPY_OBJECTS] = "Objects: Copy";
+    action_names[ACTION_PASTE_OBJECTS] = "Objects: Paste";
+    action_names[ACTION_HIDE_OBJECTS] = "Objects: Hide";
+    action_names[ACTION_SHOW_OBJECTS] = "Objects: Show";
+    action_names[ACTION_UNDO_COMMAND] = "Undo command";
+    action_names[ACTION_REDO_COMMAND] = "Redo command";
 
-static constexpr ActionNamePair action_name_map_data[] = {
-    {ACTION_MOVE_CAMERA_FORWARD,  "Camera: move forward"},
-    {ACTION_MOVE_CAMERA_BACKWARD, "Camera: move backward"},
-    {ACTION_MOVE_CAMERA_LEFT,     "Camera: move left"},
-    {ACTION_MOVE_CAMERA_RIGHT,    "Camera: move right"},
-
-    {ACTION_MOVE_OBJECTS,   "Objects: Grab"},
-    {ACTION_ROTATE_OBJECTS, "Objects: Rotate"},
-    {ACTION_SCALE_OBJECTS,  "Objects: Scale"},
-    {ACTION_COPY_OBJECTS,   "Objects: Copy"},
-    {ACTION_PASTE_OBJECTS,  "Objects: Paste"},
-    {ACTION_HIDE_OBJECTS,   "Objects: Hide"},
-    {ACTION_SHOW_OBJECTS,   "Objects: Show"},
-
-    {ACTION_UNDO_COMMAND, "Undo command"},
-    {ACTION_REDO_COMMAND, "Redo command"}
-};
-
-static_assert(sizeof action_name_map_data /
-    sizeof(ActionNamePair) == TOTAL_ACTIONS);
-
-static const ActionNameMap action_name_map(std::begin(action_name_map_data),
-    std::end(action_name_map_data));
+    return action_names;
+}
 
 const char* to_c_string(Action action)
 {
-    return action_name_map.at(action);
+    static const char* const * action_names = get_action_names();
+
+    return action_names[action];
 }
