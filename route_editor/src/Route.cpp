30e16e40d5b7d766bf94264174a12859c4ee7878
avilkinnick <avilkin.nick@gmail.com>
2026-03-13 06:34:48 +0300

    Вынес читальщик в отдельный файл

--- route_editor/src/Route.cpp
+++ route_editor/src/Route.cpp
@@ -6,6 +6,7 @@
 #include "RouteObject.h"
 #include "Settings.h"
 #include "filesystem.h"
+#include "parse_file_funcs.h"
 #include "rail-signal.h"
 #include "signals-data-types.h"
 #include "topology.h"
@@ -24,22 +25,15 @@
 
 #include <QString>
 
-// #include <algorithm>
 #include <cassert>
 #include <cmath>
-#include <cstdarg>
 #include <cstddef>
-#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
-#include <cerrno>
 #include <filesystem>
 #include <functional>
-// #include <fstream>
-// #include <sstream>
 #include <string>
 #include <utility>
-// #include <vector>
 
 #define LABEL_BUFFER_SIZE 256
 #define RELATIVE_PATH_BUFFER_SIZE 512
@@ -53,328 +47,6 @@ static vsg::vec3 to_vsg_vec3(dvec3 vec)
         static_cast<float>(vec.y),
         static_cast<float>(vec.z)
     };
-}
-
-static bool to_float(const char* buf, float* out)
-{
-    char* endptr;
-    errno = 0;
-    const float result = std::strtof(buf, &endptr);
-    if (errno == 0 && *endptr == '\0')
-    {
-        *out = result;
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-static bool to_double(const char* buf, double* out)
-{
-    char* endptr;
-    errno = 0;
-    const double result = std::strtod(buf, &endptr);
-    if (errno == 0 && *endptr == '\0')
-    {
-        *out = result;
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-static char* read_file_in_buffer(const char* filename, const char* modes)
-{
-    std::FILE* const file = std::fopen(filename, modes);
-    if (!file)
-    {
-        // TODO: Replace on Journal
-        std::fprintf(stderr, "Failed to open %s\n", filename);
-        return nullptr;
-    }
-
-    std::fseek(file, 0, SEEK_END);
-    const long buffer_length = std::ftell(file);
-    std::rewind(file);
-
-    char* const buffer = reinterpret_cast<char*>(
-        std::malloc(buffer_length + 1));
-
-    if (!buffer)
-    {
-        // TODO: Replace on Journal
-        std::fprintf(stderr, "Failed to allocate memory for %s content\n",
-            filename);
-
-        std::fclose(file);
-        return nullptr;
-    }
-
-    const std::size_t bytes_read = std::fread(buffer, 1, buffer_length, file);
-    buffer[buffer_length] = '\0';
-
-    std::fclose(file);
-
-    if (bytes_read < static_cast<std::size_t>(buffer_length))
-    {
-        // TODO: Replace on Journal
-        std::fprintf(stderr, "Failed to read %s\n", filename);
-        std::free(buffer);
-        return nullptr;
-    }
-
-    return buffer;
-}
-
-enum Type
-{
-    TYPE_STRING,
-    TYPE_FLOAT,
-    TYPE_DOUBLE
-};
-
-struct Field
-{
-    Type type;
-    char* buffer;
-    std::size_t buffer_length;
-    std::size_t buffer_size;
-    void* out;
-
-    bool process(const char* filename, int line_num, char* line_buffer)
-    {
-        buffer[buffer_length] = '\0';
-        buffer_length = 0;
-
-        switch (type)
-        {
-            case TYPE_FLOAT:
-            {
-                if (!to_float(buffer, reinterpret_cast<float*>(out)))
-                {
-                    std::fprintf(stderr, "%s:%d: error: failed to "
-                        "read float value\n    %s\n",
-                        filename, line_num, line_buffer);
-
-                    return false;
-                }
-
-                return true;
-            }
-            case TYPE_DOUBLE:
-            {
-                if (!to_double(buffer, reinterpret_cast<double*>(out)))
-                {
-                    std::fprintf(stderr, "%s:%d: error: failed to "
-                        "read double value\n    %s\n",
-                        filename, line_num, line_buffer);
-
-                    return false;
-                }
-
-                return true;
-            }
-            default:
-            {
-                return true;
-            }
-        }
-    }
-
-    bool append_char(const char* filename, int line_num,
-        char* line_buffer, char ch)
-    {
-        buffer[buffer_length] = ch;
-        ++buffer_length;
-
-        if (buffer_length == buffer_size)
-        {
-            std::fprintf(stderr, "%s:%d: error: value is not fitting into "
-                "buffer\n    %s\n", filename, line_num, line_buffer);
-
-            return false;
-        }
-
-        return true;
-    }
-};
-
-static bool read_line_by_line(const char* filename, const char* modes,
-    const char* separators, std::function<void()> function, int argc, ...)
-{
-    char* const buffer = read_file_in_buffer(filename, modes);
-    if (!buffer)
-    {
-        return false;
-    }
-
-    Field* const fields = reinterpret_cast<Field*>(
-        std::malloc(sizeof(Field) * argc));
-
-    if (!fields)
-    {
-        std::fprintf(stderr, "Failed to allocate memory for %s fields\n",
-            filename);
-
-        std::free(buffer);
-        return false;
-    }
-
-    std::va_list args;
-    va_start(args, argc);
-
-    for (int i = 0; i < argc; ++i)
-    {
-        Field* const field = &fields[i];
-        field->type = va_arg(args, Type);
-        field->buffer = va_arg(args, char*);
-        field->buffer_length = 0;
-        field->buffer_size = va_arg(args, std::size_t);
-
-        if (field->type != TYPE_STRING)
-        {
-            field->out = va_arg(args, void*);
-        }
-    }
-
-    va_end(args);
-
-    // Допусти у нас 5(N) аргументов, тогда:
-    // state 0 - до чтения первого элемента
-    // state 1 - во время чтения первого элемента
-    // state 2 - до чтения второго элемента
-    // ...
-    // state 9(2N - 1) - во время чтения пятого элемента
-    // state 2N - прочитали все, что должно быть в строчке
-
-    // Если state == 0, то пустая строка - игнорируем;
-    // Если state == 2N - 1, то последний элемент еще нужно записать;
-    // Если state == 2N, то все нормально;
-    // Иначе данные в строке неверные - выходим с ошибкой
-
-    int curr_state = 0;
-    const char* ptr = buffer;
-    int line_num = 1;
-    char line_buffer[LINE_BUFFER_SIZE];
-    int line_length = 0;
-
-/*
-0 - обновить линии, но ничего не делать
-2N - 1 - обработать значение, вызвать function, обновить линии
-2N - вызвать function, обновить линии
-другое - ошибка
-*/
-
-    while (true)
-    {
-        line_buffer[line_length] = *ptr;
-        ++line_length;
-
-        if (*ptr == '\0' || *ptr == '\n')
-        {
-            line_buffer[line_length] = '\0';
-            line_length = 0;
-
-            // Newline or EOF right after field - process field
-            if (curr_state == 2 * argc - 1)
-            {
-                if (!fields[(curr_state - 1) / 2].process(
-                    filename, line_num, line_buffer))
-                {
-                    std::free(fields);
-                    std::free(buffer);
-                    return false;
-                }
-            }
-            else if (curr_state != 0 && curr_state != 2 * argc)
-            {
-                std::fprintf(stderr, "%s:%d: error: wrong field count\n"
-                    "    %s\n", filename, line_num, line_buffer);
-
-                std::free(fields);
-                std::free(buffer);
-                return false;
-            }
-
-            if (curr_state != 0)
-            {
-                function();
-            }
-
-            ++line_num;
-            curr_state = 0;
-
-            if (*ptr == '\0')
-            {
-                break;
-            }
-        }
-        else
-        {
-            bool is_separator = false;
-            for (const char* sep = separators; *sep != '\0'; ++sep)
-            {
-                if (*ptr == *sep)
-                {
-                    is_separator = true;
-                    break;
-                }
-            }
-
-            const bool state_is_even = (curr_state % 2 == 0);
-
-            if (is_separator)
-            {
-                if (!state_is_even)
-                {
-                    if (!fields[(curr_state - 1) / 2].process(
-                        filename, line_num, line_buffer))
-                    {
-                        std::free(fields);
-                        std::free(buffer);
-                        return false;
-                    }
-
-                    ++curr_state;
-                }
-            }
-            else
-            {
-                if (curr_state == 2 * argc)
-                {
-                    std::fprintf(stderr, "%s:%d: error: too many fields in "
-                        "line\n    %s\n", filename, line_num, line_buffer);
-
-                    std::free(fields);
-                    std::free(buffer);
-                    return false;
-                }
-
-                if (state_is_even)
-                {
-                    ++curr_state;
-                }
-
-                if (!fields[(curr_state - 1) / 2].append_char(
-                    filename, line_num, line_buffer, *ptr))
-                {
-                    std::free(fields);
-                    std::free(buffer);
-                    return false;
-                }
-            }
-        }
-
-        ++ptr;
-    }
-
-    std::free(fields);
-    std::free(buffer);
-    return true;
 }
 
 Route::Route(EditorContext& context)
@@ -417,13 +89,13 @@ bool Route::load_objects_ref()
     char label[LABEL_BUFFER_SIZE];
     char relative_path[RELATIVE_PATH_BUFFER_SIZE];
 
-    return read_line_by_line(
+    return parse_file_line_by_line(
         objects_ref_path.c_str(), "r", " \t\r",
         [&]() -> void {
             context.objects_ref.emplace(label, relative_path);
         }, 2,
-        TYPE_STRING, label, static_cast<std::size_t>(LABEL_BUFFER_SIZE),
-        TYPE_STRING, relative_path, static_cast<std::size_t>(RELATIVE_PATH_BUFFER_SIZE)
+        PARSE_VALUE_TYPE_STRING, label, static_cast<std::size_t>(LABEL_BUFFER_SIZE),
+        PARSE_VALUE_TYPE_STRING, relative_path, static_cast<std::size_t>(RELATIVE_PATH_BUFFER_SIZE)
     );
 }
 
@@ -439,19 +111,19 @@ bool Route::load_route_map()
     vsg::vec3 translation;
     vsg::vec3 rotation;
 
-    return read_line_by_line(
+    return parse_file_line_by_line(
         route_map_path.c_str(), "r", " \t\r,;",
         [&]() -> void {
             context.route_map[label].emplace_back(
                 RouteMapTransformation{translation, rotation});
         }, 7,
-        TYPE_STRING, label, static_cast<std::size_t>(LABEL_BUFFER_SIZE),
-        TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &translation.x,
-        TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &translation.y,
-        TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &translation.z,
-        TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &rotation.x,
-        TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &rotation.y,
-        TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &rotation.z
+        PARSE_VALUE_TYPE_STRING, label, static_cast<std::size_t>(LABEL_BUFFER_SIZE),
+        PARSE_VALUE_TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &translation.x,
+        PARSE_VALUE_TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &translation.y,
+        PARSE_VALUE_TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &translation.z,
+        PARSE_VALUE_TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &rotation.x,
+        PARSE_VALUE_TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &rotation.y,
+        PARSE_VALUE_TYPE_FLOAT, float_buffer, static_cast<std::size_t>(FLOAT_BUFFER_SIZE), &rotation.z
     );
 }
 
--- route_editor/src/parse_file_funcs.cpp
+++ route_editor/src/parse_file_funcs.cpp
@@ -1,9 +1,51 @@
 #include "parse_file_funcs.h"
 
+#include <cstdarg>
+#include <cerrno>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <functional>
+
+static bool to_float(const char* str, float* out)
+{
+    char* endptr;
+    errno = 0;
+    const float result = std::strtof(str, &endptr);
+    if (errno == 0 && *endptr == '\0')
+    {
+        *out = result;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+static bool to_double(const char* str, double* out)
+{
+    char* endptr;
+    errno = 0;
+    const double result = std::strtod(str, &endptr);
+    if (errno == 0 && *endptr == '\0')
+    {
+        *out = result;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+static void print_error(const char* filename, int line_num, const char* error,
+    const char* line_begin, const char* line_end)
+{
+    std::fprintf(stderr, "%s:%d: error: %s\n    %.*s\n",
+        filename, line_num, error,
+        static_cast<int>(line_end - line_begin), line_begin);
+}
 
 struct ParseValue
 {
@@ -11,7 +53,40 @@ struct ParseValue
     char* buffer;
     std::size_t buffer_length;
     std::size_t buffer_size;
-    void* out_variable;
+    void* out;
+
+    bool process(const char** error)
+    {
+        buffer[buffer_length] = '\0';
+        buffer_length = 0;
+
+        switch (type)
+        {
+            case PARSE_VALUE_TYPE_FLOAT:
+            {
+                *error = "failed to read float value";
+                return to_float(buffer, reinterpret_cast<float*>(out));
+            }
+            case PARSE_VALUE_TYPE_DOUBLE:
+            {
+                *error = "failed to read double value";
+                return to_double(buffer, reinterpret_cast<double*>(out));
+            }
+            default:
+            {
+                return true;
+            }
+        }
+    }
+
+    bool append_char(char ch, const char** error)
+    {
+        buffer[buffer_length] = ch;
+        ++buffer_length;
+
+        *error = "value is not fitting into buffer";
+        return buffer_length == buffer_size;
+    }
 };
 
 char* read_file_in_buffer(const char* filename, const char* modes)
@@ -27,10 +102,14 @@ char* read_file_in_buffer(const char* filename, const char* modes)
     const long buffer_length = std::ftell(file);
     std::rewind(file);
 
-    char* const buffer = reinterpret_cast<char*>(std::malloc(buffer_length + 1));
+    char* const buffer = reinterpret_cast<char*>(
+        std::malloc(buffer_length + 1));
+
     if (!buffer)
     {
-        std::fprintf(stderr, "Failed to allocate memory for %s content\n", filename);
+        std::fprintf(stderr, "Failed to allocate memory "
+            "for %s content\n", filename);
+
         std::fclose(file);
         return nullptr;
     }
@@ -53,5 +132,148 @@ char* read_file_in_buffer(const char* filename, const char* modes)
 bool parse_file_line_by_line(const char* filename, const char* modes,
     const char* separators, std::function<void()> func, int argc, ...)
 {
+    char* const buffer = read_file_in_buffer(filename, modes);
+    if (!buffer)
+    {
+        return false;
+    }
 
+    ParseValue* const parse_values = reinterpret_cast<ParseValue*>(
+        std::malloc(sizeof(ParseValue) * argc));
+
+    if (!parse_values)
+    {
+        std::fprintf(stderr, "Failed to allocate memory "
+            "for %s parse values\n", filename);
+
+        std::free(buffer);
+        return false;
+    }
+
+    std::va_list args;
+    va_start(args, argc);
+
+    for (int i = 0; i < argc; ++i)
+    {
+        ParseValue* const parse_value = &parse_values[i];
+        parse_value->type = va_arg(args, ParseValueType);
+        parse_value->buffer = va_arg(args, char*);
+        parse_value->buffer_length = va_arg(args, std::size_t);
+
+        if (parse_value->type != PARSE_VALUE_TYPE_STRING)
+        {
+            parse_value->out = va_arg(args, void*);
+        }
+    }
+
+    va_end(args);
+
+    int curr_state = 0;
+    int line_num = 1;
+    int index;
+    const char* ptr = buffer;
+    const char* line_begin = ptr;
+    const char* error;
+
+    while (true)
+    {
+        if (*ptr == '\n' || *ptr == '\0')
+        {
+            if (curr_state == 2 * argc - 1)
+            {
+                index = (curr_state - 1) / 2;
+                if (!parse_values[index].process(&error))
+                {
+                    print_error(filename, line_num, error, line_begin, ptr);
+                    std::free(parse_values);
+                    std::free(buffer);
+                    return false;
+                }
+            }
+            else if (curr_state != 0 && curr_state != 2 * argc)
+            {
+                print_error(filename, line_num, "wrong parse value count",
+                    line_begin, ptr);
+
+                std::free(parse_values);
+                std::free(buffer);
+                return false;
+            }
+
+            if (curr_state != 0)
+            {
+                func();
+            }
+
+            if (*ptr == '\0')
+            {
+                std::free(parse_values);
+                std::free(buffer);
+                return true;
+            }
+
+            curr_state = 0;
+            ++line_num;
+            line_begin = ptr + 1;
+        }
+        else
+        {
+            bool is_separator = false;
+            for (const char* sep = separators; *sep != '\0'; ++sep)
+            {
+                if (*ptr == *sep)
+                {
+                    is_separator = true;
+                    break;
+                }
+            }
+
+            const bool state_is_even = (curr_state % 2 == 0);
+
+            if (is_separator)
+            {
+                if (!state_is_even)
+                {
+                    index = (curr_state - 1) / 2;
+                    if (!parse_values[index].process(&error))
+                    {
+                        print_error(filename, line_num, error, line_begin, ptr);
+                        std::free(parse_values);
+                        std::free(buffer);
+                        return false;
+                    }
+
+                    ++curr_state;
+                }
+            }
+            else
+            {
+                if (curr_state == 2 * argc)
+                {
+                    print_error(filename, line_num, "too many parse values"
+                        "int line", line_begin, ptr);
+
+                    std::free(parse_values);
+                    std::free(buffer);
+                    return false;
+                }
+
+                if (state_is_even)
+                {
+                    ++curr_state;
+                }
+
+                index = (curr_state - 1) / 2;
+                if (parse_values[index].append_char(*ptr, &error))
+                {
+                    print_error(filename, line_num, error, line_begin, ptr);
+                    std::free(parse_values);
+                    std::free(buffer);
+                    return false;
+                }
+            }
+        }
+
+        ++ptr;
+    }
 }
