8dfd88b17a397ab4bdc4b996a0124d6cb04cd094
avilkinnick <avilkin.nick@gmail.com>
2026-03-17 08:16:15 +0300

    Рефактор EditorContext

--- route_editor/include/EditorContext.h
+++ route_editor/include/EditorContext.h
@@ -3,22 +3,23 @@
 
 #include "CommandList.h"
 #include "EditorState.h"
-#include "Gizmo.h"
-#include "Outline.h"
-#include "Route.h"
 #include "RouteMap.h"
 #include "RouteObject.h"
 #include "Settings.h"
 
 #include <vsg/core/ref_ptr.h>
 
+#include <map>
 #include <string>
 
 class CameraHandler;
+class Gizmo;
 class IntersectionHandler;
 class KeyboardHandler;
 class MouseHandler;
 class ObjectSelector;
+class OutlineBuilder;
+class Route;
 class SceneGraph;
 class Topology;
 class WindowHandler;
@@ -39,35 +40,48 @@ class Window;
 
 struct EditorContext
 {
+    EditorContext();
     ~EditorContext();
 
     EditorState state = EditorState::SELECT_ROUTE;
     settings_t settings;
     CommandList commands;
+
     vsg::ref_ptr<vsg::Options> options;
+
     vsg::ref_ptr<vsg::Window> window;
     vsg::ref_ptr<WindowHandler> window_handler;
+
     vsg::ref_ptr<MouseHandler> mouse_handler;
     vsg::ref_ptr<KeyboardHandler> keyboard_handler;
+
     vsg::ref_ptr<vsg::Perspective> perspective;
     vsg::ref_ptr<vsg::LookAt> look_at;
     vsg::ref_ptr<vsg::Camera> camera;
     vsg::ref_ptr<CameraHandler> camera_handler;
+
     vsg::ref_ptr<IntersectionHandler> intersection_handler;
+
     vsg::ref_ptr<SceneGraph> scene_graph;
     vsg::ref_ptr<Route> route;
+
     vsg::ref_ptr<vsg::ClearAttachments> clear_attachments;
     vsg::ref_ptr<vsg::RenderGraph> render_graph;
     vsg::ref_ptr<vsg::Viewer> viewer;
+
     vsg::ref_ptr<Gizmo> gizmo;
     vsg::ref_ptr<ObjectSelector> object_selector;
-    RouteObjects selected_objects;
-    RouteObjects hidden_objects;
-    std::string route_dir;
     vsg::ref_ptr<OutlineBuilder> outline_builder;
+
     std::map<std::string, std::string> objects_ref;
     RouteMap route_map;
     Topology* topology = nullptr;
+
+    RouteObjects selected_objects;
+    RouteObjects hidden_objects;
+
+    std::string route_dir;
+
     double delta_time;
 };
 
--- route_editor/src/EditorContext.cpp
+++ route_editor/src/EditorContext.cpp
@@ -1,18 +1,27 @@
 #include "EditorContext.h"
 
-#include "ObjectSelector.h"
-#include "WindowHandler.h"
-#include "MouseHandler.h"
-#include "KeyboardHandler.h"
-#include <vsg/app/ProjectionMatrix.h>
-#include <vsg/app/Camera.h>
-#include <vsg/app/RenderGraph.h>
-#include <vsg/app/Viewer.h>
-#include <vsg/commands/ClearAttachments.h>
 #include "CameraHandler.h"
+#include "Gizmo.h"
 #include "IntersectionHandler.h"
+#include "KeyboardHandler.h"
+#include "MouseHandler.h"
+#include "ObjectSelector.h"
+#include "Outline.h"
+#include "Route.h"
 #include "SceneGraph.h"
+#include "WindowHandler.h"
 #include "topology.h"
+
+#include <vsg/app/Camera.h>
+#include <vsg/app/ProjectionMatrix.h>
+#include <vsg/app/RenderGraph.h>
+#include <vsg/app/ViewMatrix.h>
+#include <vsg/app/Viewer.h>
+#include <vsg/app/Window.h>
+#include <vsg/commands/ClearAttachments.h>
+#include <vsg/io/Options.h>
+
+EditorContext::EditorContext() = default;
 
 EditorContext::~EditorContext()
 {
