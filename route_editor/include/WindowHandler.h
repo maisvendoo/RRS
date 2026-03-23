c01484285aae63f26f91539fd4eb1299e5a8c7fe
avilkinnick <avilkin.nick@gmail.com>
2026-03-23 07:54:13 +0300

    Обработка aspectRatio в редакторе

--- route_editor/include/WindowHandler.h
+++ route_editor/include/WindowHandler.h
@@ -6,10 +6,22 @@
 
 struct EditorContext;
 
+namespace vsg
+{
+
+class ConfigureWindowEvent;
+
+}
+
 class WindowHandler : public vsg::Inherit<vsg::Visitor, WindowHandler>
 {
 public:
     explicit WindowHandler(EditorContext& context);
+
+    virtual void apply(vsg::ConfigureWindowEvent& configureWindow) override;
+
+private:
+    EditorContext& context;
 };
 
 #endif // WINDOW_HANDLER_H
