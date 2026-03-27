e6d30b201d9a73bf8f11197eb87ac4e854067ca6
avilkinnick <avilkin.nick@gmail.com>
2026-03-27 08:41:40 +0300

    Заделка под ObjectManager на будущее

---
+++ route_editor/include/ObjectManager.h
@@ -0,0 +1,50 @@
+#ifndef OBJECT_MANAGER_H
+#define OBJECT_MANAGER_H
+
+#include <vsg/core/ref_ptr.h>
+#include <vsg/maths/box.h>
+#include <vsg/maths/mat4.h>
+#include <vsg/maths/vec3.h>
+
+#include <cstddef>
+#include <string>
+
+struct EditorContext;
+class SingleSwitch;
+
+namespace vsg
+{
+
+class MatrixTransform;
+class PagedLOD;
+
+}
+
+class ObjectManager
+{
+public:
+    explicit ObjectManager(EditorContext& context, size_t max_object_count);
+    ~ObjectManager();
+
+private:
+    EditorContext& context;
+
+    vsg::ref_ptr<vsg::MatrixTransform>* transforms = nullptr;
+
+    std::string* labels = nullptr;
+    vsg::dvec3* translations = nullptr;
+    vsg::dvec3* rotation_degs = nullptr;
+    vsg::dvec3* scales = nullptr;
+
+    vsg::dmat4* initial_matrixes = nullptr;
+    vsg::dbox* bounds = nullptr;
+
+    bool* is_selected = nullptr;
+    bool* is_hidden = nullptr;
+
+    vsg::ref_ptr<SingleSwitch>* paged_lod_switches = nullptr;
+    vsg::ref_ptr<vsg::PagedLOD>* paged_lods = nullptr;
+    vsg::ref_ptr<SingleSwitch>* outline_switches = nullptr;
+};
+
+#endif // OBJECT_MANAGER_H
---
+++ route_editor/src/ObjectManager.cpp
@@ -0,0 +1,14 @@
+#include "ObjectManager.h"
+
+#include <cstddef>
+
+ObjectManager::ObjectManager(EditorContext& context, size_t max_object_count)
+    : context(context)
+{
+
+}
+
+ObjectManager::~ObjectManager()
+{
+
+}
