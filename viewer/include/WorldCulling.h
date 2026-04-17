f14bd735c5e1636eb0f2ea5b3cfc5d19abf16203
Mikhail Arepev <arepmm@gmail.com>
2026-04-18 00:27:07 +0300

    Комментарии к коду

--- viewer/include/WorldCulling.h
+++ viewer/include/WorldCulling.h
@@ -25,19 +25,30 @@ public:
 
     vsg::ref_ptr<vsg::Group> world_root;
 
+    /// Добавить node в сцену; отсечение видимости этого узла будет исходить
+    /// из расчёта, что он располагается в тайле, содержащем координату point
     void add(const vsg::dvec3 point, vsg::ref_ptr<vsg::Node> node);
 
 private:
 
+    /// Индексы тайлов
     using tile_index_t = std::tuple<int, int, int>;
+    /// Хранилище крупномасштабных узлов отсечения, сортированное по индексам тайлов
     using tile_map_0_t = std::map<tile_index_t, vsg::ref_ptr<vsg::CullGroup>>;
+    /// Хранилище мелкомасштабных узлов отсечения, сортированное по индексам тайлов;
+    /// в каждом мелкомасштабном хранилище - своё крупномасштабное
     using tile_map_1_t = std::map<tile_index_t, std::pair<vsg::ref_ptr<vsg::CullGroup>, tile_map_0_t>>;
+
     tile_map_1_t tiles;
 
+    /// Размер крупномасштабной сетки
     double tile_size_0 = 4000.0;
+    /// Размер мелкомасштабной сетки
     double tile_size_1 = 32000.0;
 
+    /// Индекс тайла для сетки размером tile_size в точке с координатами point
     tile_index_t get_index(const double tile_size, const vsg::dvec3 point) const;
+    /// Ограничивающая сфера для тайла index на сетке размером tile_size
     vsg::dsphere get_bound(const double tile_size, const tile_index_t index) const;
 };
 
--- viewer/src/WorldCulling.cpp
+++ viewer/src/WorldCulling.cpp
@@ -11,19 +11,11 @@ WorldCulling::WorldCulling(double in_tile_size_0, double in_tile_size_1)
 {
     world_root = vsg::Group::create();
 
-    if (in_tile_size_0 > 0.0)
-    {
-        tile_size_0 = in_tile_size_0;
-    }
+    // Размер крупномасштабных тайлов обязательно больше нуля
+    tile_size_0 = (in_tile_size_0 > 0.0) ? in_tile_size_0 : 4000.0;
 
-    if (in_tile_size_1 > tile_size_0)
-    {
-        tile_size_1 = in_tile_size_1;
-    }
-    else
-    {
-        tile_size_1 = 8.0 * tile_size_0;
-    }
+    // Размер мелкомасштабных тайлов обязательно больше крупномасштабных в целое число раз
+    tile_size_1 = tile_size_0 * std::max(2.0, std::ceil(in_tile_size_1 / tile_size_0));
 }
 
 //------------------------------------------------------------------------------
@@ -31,29 +23,44 @@ WorldCulling::WorldCulling(double in_tile_size_0, double in_tile_size_1)
 //------------------------------------------------------------------------------
 void WorldCulling::add(const vsg::dvec3 point, vsg::ref_ptr<vsg::Node> node)
 {
+    // Индекс мелкомасштабного тайла
     tile_index_t index_1 = get_index(tile_size_1, point);
+    // Находим мелкомасштабный тайл
     auto itr_1 = tiles.find(index_1);
     if (itr_1 == tiles.end())
     {
+        // Если мелкомасштабный тайл ещё не существует, создаём его
         const vsg::dsphere bound = get_bound(tile_size_1, index_1);
         const vsg::ref_ptr<vsg::CullGroup> tile = vsg::CullGroup::create(bound);
+
+        // Добавляем созданный тайл в сцену
         world_root->addChild(tile);
 
+        // Добавляем созданный тайл в хранилище
         itr_1 = tiles.emplace(index_1, std::make_pair(tile, tile_map_0_t{})).first;
     }
+
+    // Хранилище крупномасштабных тайлов в найденном мелкомасштабном
     tile_map_0_t& tiles_0 = itr_1->second.second;
 
+    // Индекс крупномасштабного тайла
     tile_index_t index_0 = get_index(tile_size_0, point);
+    // Находим крупномасштабный тайл
     auto itr_0 = tiles_0.find(index_0);
     if (itr_0 == tiles_0.end())
     {
+        // Если крупномасштабный тайл ещё не существует, создаём его
         const vsg::dsphere bound = get_bound(tile_size_0, index_0);
         const vsg::ref_ptr<vsg::CullGroup> tile = vsg::CullGroup::create(bound);
+
+        // Добавляем крупномасштабный тайл в сцену мелкомасштабного тайла
         itr_1->second.first->addChild(tile);
 
+        // Добавляем созданный тайл в хранилище
         itr_0 = tiles_0.emplace(index_0, tile).first;
     }
 
+    // Добавляем ноду в сцену найденного крупномасштабного тайла
     itr_0->second->addChild(node);
 }
 
