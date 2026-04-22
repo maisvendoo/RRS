12125a28abb004f84273c57d0d3c66ff27455809
avilkinnick <avilkin.nick@gmail.com>
2026-04-22 07:16:53 +0300

    Перенес реализацию route-command в .cpp

--- simulator/topology/include/route-command.h
+++ simulator/topology/include/route-command.h
@@ -1,9 +1,9 @@
 #ifndef     ROUTE_COMMAND_H
 #define     ROUTE_COMMAND_H
 
+#include    "topology-export.h"
+
 #include    <QByteArray>
-#include    <QDataStream>
-#include    <QIODevice>
 #include    <QString>
 
 #include    <cstdint>
@@ -11,7 +11,7 @@
 //------------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------------
-struct route_command_t
+struct TOPOLOGY_EXPORT route_command_t
 {
     /// Имя траектории, с которой начать поиск маршрута
     QString trajectory_begin = "";
@@ -20,26 +20,8 @@ struct route_command_t
     /// Направление маршрута по топологии: 1 - вперёд, -1 - назад
     std::int8_t dir = 0;
 
-    QByteArray serialize() const
-    {
-        QByteArray data;
-        QDataStream stream(&data, QIODevice::WriteOnly);
-
-        stream << trajectory_begin;
-        stream << trajectory_end;
-        stream << dir;
-
-        return data;
-    }
-
-    void deserialize(QByteArray& data)
-    {
-        QDataStream stream(&data, QIODevice::ReadOnly);
-
-        stream >> trajectory_begin;
-        stream >> trajectory_end;
-        stream >> dir;
-    }
+    QByteArray serialize() const;
+    void deserialize(QByteArray& data);
 };
 
 #endif // ROUTE_COMMAND_H
