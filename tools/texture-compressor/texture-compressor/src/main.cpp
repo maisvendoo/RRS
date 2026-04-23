96864ccb7c6c1f1b784f5c9559bafd4fef5d868f
maisvendoo <maisvendoo@gmail.com>
2026-04-23 16:32:25 +0300

    Подключил основные зависимости

--- tools/texture-compressor/texture-compressor/src/main.cpp
+++ tools/texture-compressor/texture-compressor/src/main.cpp
@@ -1,7 +1,11 @@
 #include    <ktx.h>
 #include    <nlohmann/json.hpp>
+#include    <stb_image.h>
 
 int main()
 {
+    int w = 0, h = 0, ch = 0;
+    stbi_uc *data = stbi_load("image.jpg", &w, &h, &ch, 0);
+
     return 0;
 }
