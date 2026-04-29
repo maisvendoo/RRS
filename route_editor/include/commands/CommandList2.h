c01e13f77e51aeda1f629b8a195ae77a8c926243
avilkinnick <avilkin.nick@gmail.com>
2026-04-29 09:59:01 +0300

    Делаю CommandList через std list

--- route_editor/include/commands/CommandList2.h
+++ route_editor/include/commands/CommandList2.h
@@ -1,4 +1,18 @@
 #ifndef COMMAND_LIST_H
 #define COMMAND_LIST_H
 
+#include <list>
+#include <memory>
+
+class Command;
+
+class CommandList
+{
+public:
+
+
+private:
+    std::list<std::unique_ptr<Command>> commands_;
+};
+
 #endif // COMMAND_LIST_H
