7571a103b286443200d2a0f4431c18893cf0cd3e
maisvendoo <maisvendoo@yandex.ru>
2026-05-10 17:22:09 +0300

    Вызов функции-распаковщика в конвертере моделей

--- tools/texture-compressor/texture-compressor/src/converter.cpp
+++ tools/texture-compressor/texture-compressor/src/converter.cpp
@@ -1,5 +1,6 @@
 #include    <converter.h>
 #include    <compressor.h>
+#include    <extractor.h>
 
 //------------------------------------------------------------------------------
 //
@@ -8,7 +9,7 @@ int Converter::run(const command_line_t &cmd_line)
 {
     if (cmd_line.extract)
     {
-        return 0;
+        return extract_models_textures(cmd_line);
     }
 
     return compress_model_textures(cmd_line);
