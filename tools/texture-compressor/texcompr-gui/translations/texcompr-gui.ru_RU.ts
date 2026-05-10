f1c8c471cad495f4ad2d663ace25691931d807e5
maisvendoo <maisvendoo@yandex.ru>
2026-05-11 00:59:06 +0300

    Обновление перевода

--- tools/texture-compressor/texcompr-gui/translations/texcompr-gui.ru_RU.ts
+++ tools/texture-compressor/texcompr-gui/translations/texcompr-gui.ru_RU.ts
@@ -15,29 +15,31 @@
     </message>
     <message>
         <location filename="../forms/mainwindow.ui" line="43"/>
+        <location filename="../forms/mainwindow.ui" line="410"/>
         <source>Model file:</source>
         <translation>Файл модели:</translation>
     </message>
     <message>
         <location filename="../forms/mainwindow.ui" line="66"/>
+        <location filename="../forms/mainwindow.ui" line="433"/>
         <source>Open model</source>
         <translation>Открыть</translation>
     </message>
     <message>
         <location filename="../forms/mainwindow.ui" line="75"/>
-        <location filename="../forms/mainwindow.ui" line="296"/>
+        <location filename="../forms/mainwindow.ui" line="299"/>
         <source>Generate mipmaps</source>
         <translation>Генерировать mipmaps-уровни</translation>
     </message>
     <message>
         <location filename="../forms/mainwindow.ui" line="85"/>
-        <location filename="../forms/mainwindow.ui" line="306"/>
+        <location filename="../forms/mainwindow.ui" line="309"/>
         <source>No rewrite existing KTX2</source>
         <translation>Не перезаписывать уже существующие KTX2</translation>
     </message>
     <message>
         <location filename="../forms/mainwindow.ui" line="95"/>
-        <location filename="../forms/mainwindow.ui" line="316"/>
+        <location filename="../forms/mainwindow.ui" line="319"/>
         <source>Overwrite GLTF file</source>
         <translation>Перезаписать GLTF-файл</translation>
     </message>
@@ -77,109 +79,123 @@
         <translation>Очистить</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="219"/>
-        <location filename="../forms/mainwindow.ui" line="360"/>
+        <location filename="../forms/mainwindow.ui" line="222"/>
+        <location filename="../forms/mainwindow.ui" line="367"/>
         <source>Compress</source>
         <translation>Сжать</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="246"/>
+        <location filename="../forms/mainwindow.ui" line="249"/>
         <source>Directory compression</source>
         <translation>Сжатие папки</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="262"/>
+        <location filename="../forms/mainwindow.ui" line="265"/>
         <source>Directory</source>
         <translation>Папка:</translation>
     </message>
     <message>
-        <location filename="../forms/mainwindow.ui" line="285"/>
+        <location filename="../forms/mainwindow.ui" line="288"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="137"/>
+        <location filename="../forms/mainwindow.ui" line="394"/>
+        <source>Extract model textures</source>
+        <translation>Извлечь текстуры модели</translation>
+    </message>
+    <message>
+        <location filename="../forms/mainwindow.ui" line="464"/>
+        <source>Extract</source>
+        <translation>Извлечь</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="145"/>
         <source>Started: %1</source>
         <translation>Запущен: %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="141"/>
+        <location filename="../src/mainwindow.cpp" line="149"/>
         <source>Failed start: %1</source>
         <translation>Сбой запуска: %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="156"/>
+        <location filename="../src/mainwindow.cpp" line="164"/>
         <source>All tasks finished success</source>
         <translation>Все задачи выполнены</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="172"/>
+        <location filename="../src/mainwindow.cpp" line="180"/>
         <source>Ready: %1</source>
         <translation>Готово: %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="176"/>
+        <location filename="../src/mainwindow.cpp" line="184"/>
         <source>Error: %1 (exit code: %2, state: %3)</source>
         <translation>Ошибка: %1 (код выхода: %2, статус: %3)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="234"/>
+        <location filename="../src/mainwindow.cpp" line="242"/>
+        <location filename="../src/mainwindow.cpp" line="621"/>
         <source>Models glTF 2.0 (*.gltf)</source>
         <translation>Модели glTF 2.9 (*.gltf)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="237"/>
+        <location filename="../src/mainwindow.cpp" line="245"/>
+        <location filename="../src/mainwindow.cpp" line="624"/>
         <source>Select model file</source>
         <translation>Выбрать файл модели</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="262"/>
+        <location filename="../src/mainwindow.cpp" line="270"/>
         <source>Image files (*.png *.jpg *.bmp *.tga)</source>
         <translation>Файлы изображений (*.png *.jpg *.bmp *.tga)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="265"/>
+        <location filename="../src/mainwindow.cpp" line="273"/>
         <source>Select texture file</source>
         <translation>Выбрать файл текстуры</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="391"/>
+        <location filename="../src/mainwindow.cpp" line="420"/>
+        <location filename="../src/mainwindow.cpp" line="435"/>
         <source>Execution success. Exit code %1</source>
         <translation>Выполнено. Код выхода %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="395"/>
+        <location filename="../src/mainwindow.cpp" line="424"/>
+        <location filename="../src/mainwindow.cpp" line="439"/>
         <source>Execution failed. Exit code %1</source>
         <translation>Ошибка выполнения. Код выхода %1</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="416"/>
-        <location filename="../src/mainwindow.cpp" line="456"/>
+        <location filename="../src/mainwindow.cpp" line="461"/>
+        <location filename="../src/mainwindow.cpp" line="501"/>
         <source>Text files (*.txt)</source>
         <translation>Тестовые файлы (*.txt)</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="418"/>
+        <location filename="../src/mainwindow.cpp" line="463"/>
         <source>Save file</source>
         <translation>Сохранить файл</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="459"/>
+        <location filename="../src/mainwindow.cpp" line="504"/>
         <source>Select text file</source>
         <translation>Выбрать файл</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="498"/>
+        <location filename="../src/mainwindow.cpp" line="543"/>
         <source>Select models folder</source>
         <translation>Выбрать каталог с моделями</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="533"/>
+        <location filename="../src/mainwindow.cpp" line="578"/>
         <source>There are no GLTF files. Abort</source>
         <translation>Отсутствуют GLTF файлы. Прервано</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.cpp" line="546"/>
+        <location filename="../src/mainwindow.cpp" line="591"/>
         <source>Find %1 files for compression...</source>
         <translation>Найдено %1 файлов моделей для сжатия текстур...</translation>
     </message>
