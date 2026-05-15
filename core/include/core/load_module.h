c8783826c9451b2eeb93c2116fcdf5fb63440a1f
avilkinnick <avilkin.nick@gmail.com>
2026-05-15 07:48:26 +0300

    core: Заменил шаблоны на макросы и перегруженные функции

--- core/include/core/load_module.h
+++ core/include/core/load_module.h
@@ -1,32 +1,24 @@
 #ifndef CORE_LOAD_MODULE_H
 #define CORE_LOAD_MODULE_H
 
-#include "core/string_funcs.h"
+#include <string>
+#include <string_view>
 
-#include <QLibrary>
+class QString;
 
-template <typename T, typename StringType>
-T* (*load_get_module_func(const StringType& lib_path))()
-{
-    QLibrary lib{to_qstring(lib_path)};
-    if (!lib.load())
-    {
-        return nullptr;
-    }
+#define LOAD_MODULE(ClassType, lib_path)    \
+    (ClassType*)load_module(lib_path)
 
-    return (T* (*)())lib.resolve("get_module");
-}
+using GetModuleFuncPtr = void* (*)();
 
-template <typename T, typename StringType>
-T* load_module(const StringType& lib_path)
-{
-    const auto* get_module_func{load_get_module_func<T>(lib_path)};
-    if (!get_module_func)
-    {
-        return nullptr;
-    }
+GetModuleFuncPtr    load_get_module_func(const char* lib_path);
+GetModuleFuncPtr    load_get_module_func(const std::string& lib_path);
+GetModuleFuncPtr    load_get_module_func(std::string_view lib_path);
+GetModuleFuncPtr    load_get_module_func(const QString& lib_path);
 
-    return get_module_func();
-}
+void*               load_module(const char* lib_path);
+void*               load_module(const std::string& lib_path);
+void*               load_module(std::string_view lib_path);
+void*               load_module(const QString& lib_path);
 
 #endif // CORE_LOAD_MODULE_H
