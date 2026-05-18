48133c30dc4df5a77599611d04d857eab9bebe54
avilkinnick <avilkin.nick@gmail.com>
2026-05-18 07:30:58 +0300

    Заменил загрузку AutoTrainStop

--- simulator/device/src/automatic-train-stop.cpp
+++ simulator/device/src/automatic-train-stop.cpp
@@ -251,25 +251,3 @@ void AutoTrainStop::stepKeysControl(double t, double dt)
         prev_key = false; // Запоминаем, что клавиша отпущена
     }
 }
-
-//------------------------------------------------------------------------------
-//
-//------------------------------------------------------------------------------
-AutoTrainStop* loadAutoTrainStop(QString lib_path)
-{
-    AutoTrainStop* autostop = nullptr;
-
-    QLibrary lib(lib_path);
-
-    if (lib.load())
-    {
-        GetAutoTrainStop getAutoTrainStop = reinterpret_cast<GetAutoTrainStop>(lib.resolve("getAutoTrainStop"));
-
-        if (getAutoTrainStop)
-        {
-            autostop = getAutoTrainStop();
-        }
-    }
-
-    return autostop;
-}
