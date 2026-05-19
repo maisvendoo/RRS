69ba6d57aff46da43bdca6fcb317941ea3ea7517
Mikhail Arepev <arepmm@gmail.com>
2026-05-19 19:21:06 +0300

    УКБМ проверяет скорость с запасом 1 кмч

--- addons/vl60/vl60-equipment/src/alsn-ukbm.cpp
+++ addons/vl60/vl60-equipment/src/alsn-ukbm.cpp
@@ -62,7 +62,7 @@ void SafetyDevice::preStep(state_vector_t &Y, double t)
         epk_state.reset();
 
         // Не даем возможности отбить свисток только при превышении 20 км/ч
-        if (v_kmh > 20.0)
+        if (v_kmh > 21.0)
         {
             return;
         }
@@ -78,7 +78,7 @@ void SafetyDevice::preStep(state_vector_t &Y, double t)
 
     if (is_lamp_on(RED_YELLOW_LAMP))
     {
-        if (v_kmh > 60.0)
+        if (v_kmh > 61.0)
         {
             setPSS();
             epk_state.reset();
@@ -93,7 +93,7 @@ void SafetyDevice::preStep(state_vector_t &Y, double t)
 
     if (is_lamp_on(YELLOW_LAMP))
     {
-        if (v_kmh > 60.0)
+        if (v_kmh > 61.0)
         {
             if (!safety_timer->isStarted())
                 safety_timer->start();
