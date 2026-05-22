5b5ada7e8eab47a6e4b699403e7b2640bb8fdabc
avilkinnick <avilkin.nick@gmail.com>
2026-05-22 08:24:41 +0300

    Revert "Редактор: Временно закомментировал всё в RouteEditor, чтобы немного переделать. Также добавил Journal и отдельный файл editor.log"
    This reverts commit 9c6ffb63b91c41807354895ca7f1dd447fd5ad69.

--- route_editor/src/main.cpp
+++ route_editor/src/main.cpp
@@ -1,28 +1,16 @@
-// #include "RouteEditor.h"
-
-// #include <cstdlib>
-
-// int main()
-// {
-//     RouteEditor route_editor;
-//     if (!route_editor.initialize())
-//     {
-//         return EXIT_FAILURE;
-//     }
-
-//     route_editor.run();
-
-//     return EXIT_SUCCESS;
-// }
-
-
 #include "RouteEditor.h"
 
 #include <cstdlib>
 
 int main()
 {
-    RouteEditor route_editor{};
+    RouteEditor route_editor;
+    if (!route_editor.initialize())
+    {
+        return EXIT_FAILURE;
+    }
+
+    route_editor.run();
 
     return EXIT_SUCCESS;
 }
