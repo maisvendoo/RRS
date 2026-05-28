e27ee44762e5bcd1b7fe04caf0fcdb7bdfaa7d17
avilkinnick <avilkin.nick@gmail.com>
2026-05-28 06:53:43 +0300

    Редактор: Делаю камеру

--- route_editor2/include/editor/Camera.h
+++ route_editor2/include/editor/Camera.h
@@ -3,9 +3,31 @@
 
 #include <vsg/app/Camera.h>
 #include <vsg/core/Inherit.h>
+#include <vsg/core/ref_ptr.h>
+
+namespace vsg
+{
+
+class LookAt;
+class Orthographic;
+class Perspective;
+
+}
 
 class Camera : public vsg::Inherit<vsg::Camera, Camera>
 {
+public:
+    Camera();
+    ~Camera();
+
+    const vsg::ref_ptr<vsg::Perspective>& get_perspective() const;
+    const vsg::ref_ptr<vsg::Orthographic>& get_orthographic() const;
+    const vsg::ref_ptr<vsg::LookAt>& get_look_at() const;
+
+private:
+    vsg::ref_ptr<vsg::Perspective> perspective;
+    vsg::ref_ptr<vsg::Orthographic> orthographic;
+    vsg::ref_ptr<vsg::LookAt> look_at;
 };
 
 #endif // EDITOR_CAMERA_H
--- route_editor2/src/Camera.cpp
+++ route_editor2/src/Camera.cpp
@@ -1,1 +1,26 @@
 #include "editor/Camera.h"
+
+#include <vsg/app/ProjectionMatrix.h>
+#include <vsg/app/ViewMatrix.h>
+#include <vsg/core/ref_ptr.h>
+
+Camera::Camera()
+{
+}
+
+Camera::~Camera() = default;
+
+const vsg::ref_ptr<vsg::Perspective>& Camera::get_perspective() const
+{
+    return perspective;
+}
+
+const vsg::ref_ptr<vsg::Orthographic>& Camera::get_orthographic() const
+{
+    return orthographic;
+}
+
+const vsg::ref_ptr<vsg::LookAt>& Camera::get_look_at() const
+{
+    return look_at;
+}
