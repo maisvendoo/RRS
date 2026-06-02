835dbc3610c29e3bb151e227e50a3662587581c9
avilkinnick <avilkin.nick@gmail.com>
2026-06-02 09:34:15 +0300

    Редактор: дополнил документацию

--- route_editor2/include/editor/Camera.h
+++ route_editor2/include/editor/Camera.h
@@ -24,12 +24,15 @@ public:
     /**
      * @brief Construct a new Camera object.
      *
-     * Create perspective and orthographic projection matrices, as well as a
-     * LookAt view matrix, based on the camera_settings and window_extent
-     * parameters. Then set the projection matrix to perspective mode.
+     * Create perspective and orthographic projection matrices, as well
+     * as a LookAt view matrix, and ViewportState based on the
+     * camera_settings and window_extent parameters. Then
+     * set the projection matrix to perspective mode.
      *
      * @param[in] camera_settings The camera settings object.
      * @param[in] window_extent Window extent from the VSG window object.
+     * @param[out] success true if initialization was successful,
+     *                     false - otherwise.
      */
     Camera(
         const camera_settings_t& camera_settings,
@@ -74,15 +77,44 @@ private:
     vsg::ref_ptr<vsg::LookAt> look_at;
 
 private:
+    /**
+     * @brief Create a perspective object.
+     *
+     * @param[in] camera_settings The camera settings object.
+     * @param[in] window_extent Window extent from the VSG window object.
+     * @return true if perspective was created successfully.
+     * @return false - otherwise.
+     */
     bool create_perspective(
         const camera_settings_t& camera_settings,
         VkExtent2D window_extent
     );
 
+    /**
+     * @brief Create an orthographic object.
+     *
+     * @param camera_settings The camera settings object.
+     * @return true if orthographic was created successfully.
+     * @return false - otherwise.
+     */
     bool create_orthographic(const camera_settings_t& camera_settings);
 
+    /**
+     * @brief Create a LookAt object.
+     *
+     * @param camera_settings The camera settings object.
+     * @return true if LookAt was created successfully.
+     * @return false - otherwise.
+     */
     bool create_look_at(const camera_settings_t& camera_settings);
 
+    /**
+     * @brief Create a ViewportState object.
+     *
+     * @param window_extent Window extent from the VSG window object.
+     * @return true if ViewportState was created successfully.
+     * @return false - otherwise.
+     */
     bool create_viewport_state(VkExtent2D window_extent);
 };
 
