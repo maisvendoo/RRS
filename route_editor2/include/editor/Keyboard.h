09b574835c7406aeac3086fab09bda2747d85ae7
avilkinnick <avilkin.nick@gmail.com>
2026-06-03 07:50:07 +0300

    Редактор: Добавил функцию для проверки нажатия кнопки только один раз

--- route_editor2/include/editor/Keyboard.h
+++ route_editor2/include/editor/Keyboard.h
@@ -2,10 +2,27 @@
 #define EDITOR_KEYBOARD_H
 
 #include <vsg/core/Inherit.h>
+#include <vsg/ui/KeyEvent.h>
 #include <vsg/ui/Keyboard.h>
 
 class Keyboard : public vsg::Inherit<vsg::Keyboard, Keyboard>
 {
+public:
+    /**
+     * @brief Check if key is pressed once (it is not held down for some time).
+     *
+     * @param[in] key The keyboard key.
+     * @param[in] ignore_handled_keys Ignore keys the already were handled by
+     *            other handlers.
+     * @return true if key is pressed only once.
+     * @return false - otherwise.
+     */
+    bool pressed_once(vsg::KeySymbol key, bool ignore_handled_keys = true) const;
+
+private:
+    using vsg::Keyboard::KeyHistory;
+    using vsg::Keyboard::keyState;
+    using vsg::Keyboard::times;
 };
 
 #endif // EDITOR_KEYBOARD_H
