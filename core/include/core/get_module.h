36889a9b4559b82329b31af318999ac4fb73d740
maisvendoo <maisvendoo@gmail.com>
2026-06-08 11:25:25 +0300

    Удалил лишнее включение

--- core/include/core/get_module.h
+++ core/include/core/get_module.h
@@ -1,7 +1,6 @@
 #ifndef CORE_GET_MODULE_H
 #define CORE_GET_MODULE_H
 
-
 #define GET_MODULE(ClassName)                      \
     extern "C" void* get_module()    \
     {                                              \
