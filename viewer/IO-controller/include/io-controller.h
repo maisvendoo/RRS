8f3ec34281f9dc19fc62fbf2e3e6884d6eddf69d
maisvendoo <maisvendoo@gmail.com>
2026-06-09 14:46:37 +0300

    Накидываю примерную структуру логики работы модуля воода/вывода

--- viewer/IO-controller/include/io-controller.h
+++ viewer/IO-controller/include/io-controller.h
@@ -3,11 +3,12 @@
 
 #include    <io-controller-export.h>
 #include    <QObject>
+#include    <set>
 
 //------------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------------
-class IOController : public QObject
+class IO_CONTROLLER_EXPORT IOController : public QObject
 {
     Q_OBJECT
 
@@ -17,9 +18,35 @@ public:
 
     ~IOController() = default;
 
+    void setPressedKey(uint16_t keyBase);
+
+    void setReleasedKey(uint16_t keyBase);
+
 protected:
 
+    /// Массив нажатых клавиш
+    std::set<uint16_t> _pressed_keys;
 
+    enum ControlType
+    {
+        CTRL_TYPE_KEYBOARD,
+        CTRL_TYPE_MOUSE,
+        CTRL_TYPE_CTRL_PANEL
+    };
+
+    /// Обработка клавиатурного управления
+    virtual void processKeyBoardInput();
+
+    /// Обработка управления мышью
+    virtual void processMouseInput();
+
+    /// Обработка управления с пульта тренажера
+    virtual void processControlPanelInput();
+
+private:
+
+    /// Обработка управления
+    void processControl(const ControlType &ctrl_type);
 };
 
 #endif
