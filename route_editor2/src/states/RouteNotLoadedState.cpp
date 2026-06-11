ee00d410713606f5e4a1869ad3568257ce4cdb61
avilkinnick <avilkin.nick@gmail.com>
2026-06-11 07:33:36 +0300

    Редактор: Кнопка загрузить маршрут в  статус-баре

--- route_editor2/src/states/RouteNotLoadedState.cpp
+++ route_editor2/src/states/RouteNotLoadedState.cpp
@@ -1,5 +1,8 @@
 #include "editor/states/RouteNotLoadedState.h"
 
+#include <filesystem.h>
+
+#include <ImGuiFileDialog.h>
 #include <vsgImGui/imgui.h>
 
 RouteNotLoadedState::~RouteNotLoadedState() = default;
@@ -7,4 +10,12 @@ RouteNotLoadedState::~RouteNotLoadedState() = default;
 void RouteNotLoadedState::fill_status_bar() const
 {
     ImGui::Text("Route is not loaded");
+    ImGui::SameLine();
+    if (ImGui::Button("Load route"))
+    {
+        IGFD::FileDialogConfig config;
+        config.path = FileSystem::getInstance().getRouteRootDir();
+        ImGuiFileDialog::Instance()->OpenDialog("LoadRouteKey",
+            "Load route", nullptr, config);
+    }
 }
