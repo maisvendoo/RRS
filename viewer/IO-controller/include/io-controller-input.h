3fe69e5ee05d92e0464edabdcba6d5d86908aeb2
maisvendoo <maisvendoo@gmail.com>
2026-06-11 10:23:55 +0300

    Структура описания органа управления в I/O-модуле

---
+++ viewer/IO-controller/include/io-controller-input.h
@@ -0,0 +1,59 @@
+#ifndef     IO_CONTROLLER_INPUT_H
+#define     IO_CONTROLLER_INPUT_H
+
+#include    <QString>
+#include    <QByteArray>
+#include    <QDataStream>
+#include    <QIODevice>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+struct io_control_input_t
+{
+    /// Индекс управляемой ПЕ
+    int controlled_vehicle_idx = 0;
+    /// Идентификатор сигнала управления в массиве сигналов ПЕ
+    uint16_t id = 0;
+    /// Значение управляющего сигнала
+    float value = 0.0f;
+
+    /// Имя назначенной клавиши
+    QString keyName = "";
+    /// Имя объекта в 3D-модели
+    QString contolledObjectName = "";
+
+    io_control_input_t()
+    {
+
+    }
+
+    QByteArray serialize() const
+    {
+        QByteArray data;
+        QDataStream stream(&data, QIODevice::WriteOnly);
+
+        // Серверу передаем только эти значения
+        stream << controlled_vehicle_idx;
+        stream << id;
+        stream << value;
+
+        return data;
+    }
+
+    void deserialize(QByteArray &data)
+    {
+        QDataStream stream(&data, QIODevice::ReadOnly);
+
+        stream >> controlled_vehicle_idx;
+        stream >> id;
+        stream >> value;
+    }
+
+    bool toBool() const
+    {
+        return static_cast<bool>(value);
+    }
+};
+
+#endif
