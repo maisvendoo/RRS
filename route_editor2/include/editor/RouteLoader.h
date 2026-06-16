7f38aa157fb434d32933937f525beab635b9872e
avilkinnick <avilkin.nick@gmail.com>
2026-06-16 07:15:31 +0300

    Редактор: Делаю нормальную асинхронную загрузку

--- route_editor2/include/editor/RouteLoader.h
+++ route_editor2/include/editor/RouteLoader.h
@@ -2,11 +2,25 @@
 #define EDITOR_ROUTE_LOADER_H
 
 #include <string>
+#include <thread>
 
 class RouteLoader
 {
 public:
+    std::thread load_static_objects_thread;
+    std::thread load_topology_thread;
+
+public:
     void start_load_route(const std::string& route_dir);
+
+private:
+    void load_static_objects(const std::string& route_dir);
+
+    bool load_objects_ref(const std::string& route_dir);
+
+    void load_route_map(const std::string& route_dir);
+
+    void load_topology(const std::string& route_dir);
 };
 
 #endif // EDITOR_ROUTE_LOADER_H
--- route_editor2/src/RouteLoader.cpp
+++ route_editor2/src/RouteLoader.cpp
@@ -1,7 +1,70 @@
 #include "editor/RouteLoader.h"
 
+#include <Journal.h>
+#include <core/string_funcs.h>
+#include <filesystem.h>
+
+#include <QString>
+
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <thread>
 
 void RouteLoader::start_load_route(const std::string& route_dir)
+{
+    load_static_objects_thread = std::thread([this, &route_dir]() -> void {
+        load_static_objects(route_dir);
+    });
+
+    load_topology_thread = std::thread([this, &route_dir]() -> void {
+        load_topology(route_dir);
+    });
+}
+
+void RouteLoader::load_static_objects(const std::string& route_dir)
+{
+    load_objects_ref(route_dir);
+    load_route_map(route_dir);
+}
+
+bool RouteLoader::load_objects_ref(const std::string& route_dir)
+{
+    const FileSystem& fs = FileSystem::getInstance();
+
+    const std::string objects_ref_path = fs.combinePath(route_dir, "objects.ref");
+
+    std::ifstream objects_ref_file(objects_ref_path);
+    if (!objects_ref_file)
+    {
+        Journal::instance()->error(QString("Failed to open objects.ref file %1")
+            .arg(to_qstring(objects_ref_path)));
+
+        return false;
+    }
+
+    Journal::instance()->info(QString("objects.ref file %1 is successfully opened")
+        .arg(to_qstring(objects_ref_path)));
+
+    std::string line;
+    while (std::getline(objects_ref_file, line))
+    {
+        std::istringstream iss(std::move(line));
+        std::string label, relative_path;
+        if (iss >> label >> relative_path)
+        {
+            Journal::instance()->info(QString("label: \"%1\"; relative_path: \"%2\"")
+                .arg(label.c_str()).arg(relative_path.c_str()));
+        }
+    }
+
+    return true;
+}
+
+void RouteLoader::load_route_map(const std::string& route_dir)
+{
+}
+
+void RouteLoader::load_topology(const std::string& route_dir)
 {
 }
