feeb9dc521d3ec732b4a69fd256bebc07805fed3
avilkinnick <avilkin.nick@gmail.com>
2026-07-01 06:03:22 +0300

    Revert "Редактор: Пробую  использовать extern template"
    This reverts commit 7c8d788ca55aee5f839109aca4b5bf3e326b0b2e.

--- route_editor2/src/template_implementations.cpp
+++ route_editor2/src/template_implementations.cpp
@@ -1,14 +1,5 @@
 #include "editor/Action.h"
-#include "editor/Keyboard.h"
-
-#include <vsg/app/ProjectionMatrix.h>
-#include <vsg/app/ViewMatrix.h>
-#include <vsg/core/ref_ptr.h>
 
 #include <array>
 
 template class std::array<const char*, TOTAL_ACTIONS>;
-template class vsg::ref_ptr<Keyboard>;
-template class vsg::ref_ptr<vsg::LookAt>;
-template class vsg::ref_ptr<vsg::Orthographic>;
-template class vsg::ref_ptr<vsg::Perspective>;
