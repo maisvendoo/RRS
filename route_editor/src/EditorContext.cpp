d0fcffa52943a082b818eeb5ea922fb718111f4f
avilkinnick <avilkin.nick@gmail.com>
2026-07-21 09:22:21 +0300

    Редактор: Убрал лишние #include

--- route_editor/src/EditorContext.cpp
+++ route_editor/src/EditorContext.cpp
@@ -3,8 +3,6 @@
 #include "Camera.h"
 #include "Gizmo.h"
 #include "IntersectionHandler.h"
-#include "Keyboard.h"
-#include "Mouse.h"
 #include "ObjectSelector.h"
 #include "Outline.h"
 #include "Route.h"
@@ -12,9 +10,6 @@
 #include "topology.h"
 #include "WindowHandler.h"
 
-#include <vsg/app/Camera.h>
-#include <vsg/app/ProjectionMatrix.h>
-#include <vsg/app/ViewMatrix.h>
 #include <vsg/commands/ClearAttachments.h>
 #include <vsg/nodes/PagedLOD.h>
 
