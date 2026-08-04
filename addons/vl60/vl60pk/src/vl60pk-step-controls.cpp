bcf5ebb0a4c73f2f524a1104d33fee12c132dc0a
maisvendoo <maisvendoo@gmail.com>
2026-08-04 16:30:26 +0300

    Применил тернарный оператор к тумблерам, для компактности

--- addons/vl60/vl60pk/src/vl60pk-step-controls.cpp
+++ addons/vl60/vl60pk/src/vl60pk-step-controls.cpp
@@ -9,36 +9,12 @@ void VL60pk::stepControls(const double &t, const double &dt)
     for (auto cab_idx : {CAB1, CAB2})
     {
         bool is_pnt = static_cast<bool>(control_inputs[cab_idx][CTRL_TUMBLER_PNT]);
-
-        if (is_pnt)
-        {
-            pants_tumbler[cab_idx].set();
-        }
-        else
-        {
-            pants_tumbler[cab_idx].reset();
-        }
+        is_pnt ? pants_tumbler[cab_idx].set() : pants_tumbler[cab_idx].reset();
 
         bool is_pnt1 = static_cast<bool>(control_inputs[cab_idx][CTRL_TUMBLER_PNT1]);
-
-        if (is_pnt1)
-        {
-            pant1_tumbler[cab_idx].set();
-        }
-        else
-        {
-            pant1_tumbler[cab_idx].reset();
-        }
+        is_pnt1 ? pant1_tumbler[cab_idx].set() : pant1_tumbler[cab_idx].reset();
 
         bool is_pnt2 = static_cast<bool>(control_inputs[cab_idx][CTRL_TUMBLER_PNT2]);
-
-        if (is_pnt2)
-        {
-            pant2_tumbler[cab_idx].set();
-        }
-        else
-        {
-            pant2_tumbler[cab_idx].reset();
-        }
+        is_pnt2 ? pant2_tumbler[cab_idx].set() : pant2_tumbler[cab_idx].reset();
     }
 }
