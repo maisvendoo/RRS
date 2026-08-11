3cc1ae1281127e557046966270f6b7735a04f8b5
maisvendoo <maisvendoo@yandex.ru>
2026-08-11 14:40:28 +0300

    Заготовка под виджет-переключалку поездов для вьювера

---
+++ viewer/include/TrainsListWidget.h
@@ -0,0 +1,48 @@
+#ifndef     TRAINS_LIST_WIDGET
+#define     TRAINS_LIST_WIDGET
+
+#include    <VehiclesHandler.h>
+#include    <vsgImGui/imgui.h>
+#include    <QString>
+#include    <vector>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+struct TrainsListWidgetParams
+{
+    VehiclesHandler *vehicles_handler = nullptr;
+    bool is_show = false;
+};
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+class TrainsListWidget
+{
+public:
+
+    TrainsListWidget(TrainsListWidgetParams *params);
+
+    ~TrainsListWidget() = default;
+
+    void show();
+
+private:
+
+    TrainsListWidgetParams* _params;
+
+    std::vector<int> _cached_trains_ids;
+
+    int _selected_train_id = -1;
+
+    bool _initialized = false;
+
+    void updateCachedTrainsList();
+
+    void renderTrainsList();
+
+    void selectTrain(int train_id);
+};
+
+#endif
