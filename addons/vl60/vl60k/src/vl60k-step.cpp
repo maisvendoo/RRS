82003bed3735a96140cc193e140abdf21a365239
maisvendoo <maisvendoo@yandex.ru>
2026-08-18 20:37:35 +0300

    vl60k: guard режима в slotAutoStart для исключения конфликта с автоостановом

--- addons/vl60/vl60k/src/vl60k-step.cpp
+++ addons/vl60/vl60k/src/vl60k-step.cpp
@@ -25,6 +25,9 @@
 //------------------------------------------------------------------------------
 void VL60k::slotAutoStart()
 {
+    if (autostart_mode != AUTOSTART_ON)
+        return;
+
     if (start_count < triggers.size())
     {
         if ((triggers[start_count] == &gv_tumbler[autostart_cab]) &&
