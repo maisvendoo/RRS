05cd7227f4e95c1aae72dc71d41e818ef3f55b57
avilkinnick <avilkin.nick@gmail.com>
2026-08-31 08:20:55 +0300

    Редактор: Делаю ObjectManager

--- route_editor/include/ObjectManager.h
+++ route_editor/include/ObjectManager.h
@@ -2,23 +2,27 @@
 #define EDITOR_OBJECT_MANAGER_H
 
 #include <vsg/core/ref_ptr.h>
+#include <vsg/maths/mat4.h>
 
 #include <cstddef>
+#include <string>
 #include <vector>
 
 namespace vsg
 {
 
 class MatrixTransform;
-class PagedLOD;
 
 }
 
 class ObjectManager
 {
 public:
-    std::vector<vsg::ref_ptr<vsg::PagedLOD>> paged_lods;
+    std::vector<std::string> labels;
+    std::vector<std::string> relative_paths;
     std::vector<vsg::ref_ptr<vsg::MatrixTransform>> matrix_transforms;
+    std::vector<vsg::dmat4> initial_matrixes;
+    std::vector<bool> is_selected;
 
 public:
     ObjectManager(std::size_t max_object_count);
--- route_editor/src/ObjectManager.cpp
+++ route_editor/src/ObjectManager.cpp
@@ -1,12 +1,14 @@
 #include "ObjectManager.h"
 
 #include <vsg/nodes/MatrixTransform.h>
-#include <vsg/nodes/PagedLOD.h>
 
 #include <cstddef>
 
 ObjectManager::ObjectManager(std::size_t max_object_count)
 {
-    paged_lods.reserve(max_object_count);
+    labels.reserve(max_object_count);
+    relative_paths.reserve(max_object_count);
     matrix_transforms.reserve(max_object_count);
+    initial_matrixes.reserve(max_object_count);
+    is_selected.reserve(max_object_count);
 }
