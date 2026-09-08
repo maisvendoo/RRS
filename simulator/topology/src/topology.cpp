dc16e93bb2830e4a5a4cdcb265928173f1f6ed0e
maisvendoo <maisvendoo@yandex.ru>
2026-09-08 20:33:52 +0300

    Revert "профиль: фильтр попутных сигналов по направлению движения (signal_dir == kind)"
    This reverts commit 5e52f89a7990261e1d48daf66d0c1ff872e3a00c.

--- simulator/topology/src/topology.cpp
+++ simulator/topology/src/topology.cpp
@@ -2390,8 +2390,7 @@ namespace
                 if ((signal != nullptr) && (matched_traj != nullptr)
                     && (signal_traj == matched_traj)
                     && (!signal->getSignalModel().isEmpty())
-                    && (!signal->getSignalModel().startsWith("empty_"))
-                    && (signal->getDirection() == static_cast<std::int8_t>(kind)))
+                    && (!signal->getSignalModel().startsWith("empty_")))
                 {
                     profile_signal_t ps;
                     ps.distance = kind * traveled;
