bac8cd7caeb92685253d806b3a1b24d42fe32ac7
maisvendoo <maisvendoo@yandex.ru>
2026-09-14 23:37:22 +0300

    Слайс 5: документация reverseTrain/actionReverseTrain + пример reverse_train

---
+++ routes/experimental-polygon_v2.0/scenarios/reverse_train/main.lua
@@ -0,0 +1,26 @@
+--[[
+
+Пример разворота поезда с использованием
+временных триггеров
+
+]]
+
+-- Задаем время начала игры
+setTime("17:30")
+
+-- Описываем поезд игрока
+my_train = TrainData.new()
+my_train.name = "ВЛ60пк"
+my_train.config = "vl60pk-1543"
+my_train.traj = "track_a_p1a"
+my_train.coord = 1690.0
+my_train.dir = 1
+
+-- Устанавливаем поезд игрока
+setTrain(my_train)
+
+-- Через 10 секунд после старта разворачиваем поезд
+setTimeTrigger("+00:00:10", actionReverseTrain(my_train.name))
+
+-- Ещё через 10 секунд снова разворачиваем — поезд возвращается к исходному направлению
+setTimeTrigger("+00:00:20", actionReverseTrain(my_train.name))
