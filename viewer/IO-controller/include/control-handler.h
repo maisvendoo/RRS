a5a43db0ad240d6d2c1867592743ad6a9d48372f
maisvendoo <maisvendoo@gmail.com>
2026-09-25 15:21:36 +0300

    Базовый класс обработчика контрола

--- viewer/IO-controller/include/control-handler.h
+++ viewer/IO-controller/include/control-handler.h
@@ -3,6 +3,13 @@
 
 #include    <QObject>
 
+#include    <set>
+
+#include    <dual-key-hash.h>
+#include    <io-controller-input.h>
+
+class CfgReader;
+
 //------------------------------------------------------------------------------
 //
 //------------------------------------------------------------------------------
@@ -19,13 +26,58 @@ public:
 
     virtual ~ControlHandler() = default;
 
+    void setFeedbackSignals(const std::vector<float> *feedback_signals)
+    {
+        this->feedback_signals = feedback_signals;
+    }
+
+    void setAnimationSignalsMap(const QMap<QString, uint16_t> *animation_signals_map)
+    {
+        this->animation_signals_map = animation_signals_map;
+    }
+
+    void setControlInputs(std::vector<DualKeyHash<uint16_t, QString, io_control_input_t>>* inputs)
+    {
+        ctrl_inputs = inputs;
+    }
+
+    /// Загрузка конфигурации из IOControllerConfig
+    virtual bool load_config(CfgReader &cfg, int cabs_num);
+
+    /// Обработка клавиатурного ввода
+    virtual void processKeyInput(const std::set<uint16_t>& pressed_keys,
+                                 int cabine_idx, int vehicle_idx) = 0;
+
+    /// Обработка мышиного ввода
+    virtual void processMouseInput(const io_control_input_t& input,
+                                   uint32_t button, bool is_pressed) = 0;
+
+    virtual void step(float dt) {};
+
 signals:
 
     void sigSendControlCommand(const QByteArray &data);
 
 protected:
 
+    /// Указатель на DualKeyHash IOController (данные и состояние)
+    std::vector<DualKeyHash<uint16_t, QString, io_control_input_t>>* ctrl_inputs = nullptr;
 
+    const std::vector<float> *feedback_signals = nullptr;
+
+    const QMap<QString, uint16_t> *animation_signals_map = nullptr;
+
+    /// Получить текущее значение сигнала по имени 3D-объекта
+    float getSignalValueByName(const QString& objectName) const;
+
+    /// Получить текущее значение сигнала по ID контрола
+    float getSignalValueByID(uint16_t control_id, int cab_idx) const;
+
+    bool getKeyState(const std::set<uint16_t>& keys, uint16_t key);
+
+    bool isKeyModifier(const std::set<uint16_t>& keys, const QString& modName);
+
+    void sendControlSignal(int vehicle_idx, int cab_idx, uint16_t id, float value);
 };
 
 #endif
--- viewer/IO-controller/src/control-handler.cpp
+++ viewer/IO-controller/src/control-handler.cpp
@@ -1,1 +1,112 @@
 #include    <control-handler.h>
+#include    <io-controller-keymap.h>
+#include    <CfgReader.h>
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+bool ControlHandler::load_config(CfgReader &cfg, int cabs_num)
+{
+    return true;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+float ControlHandler::getSignalValueByName(const QString &objectName) const
+{
+    if (objectName.isEmpty() || feedback_signals == nullptr)
+    {
+        return 0.0f;
+    }
+
+    auto it = animation_signals_map->find(objectName);
+
+    if (it != animation_signals_map->end())
+    {
+        uint16_t signal_id = it.value();
+
+        if (signal_id < feedback_signals->size())
+        {
+            float state = (*feedback_signals)[signal_id];
+            return state;
+        }
+    }
+
+    return 0.0f;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+float ControlHandler::getSignalValueByID(uint16_t control_id, int cab_idx) const
+{
+    auto ic_input = (*ctrl_inputs)[cab_idx].getByKey1(control_id);
+
+    if (!ic_input)
+    {
+        return 0.0f;
+    }
+
+    return getSignalValueByName(ic_input->contolledObjectName);
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+bool ControlHandler::getKeyState(const std::set<uint16_t> &keys, uint16_t key)
+{
+    if (key == 0 || key == KEY_Undefined)
+    {
+        return false;
+    }
+
+    return keys.find(key) != keys.end();
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+bool ControlHandler::isKeyModifier(const std::set<uint16_t> &keys,
+                                   const QString &modName)
+{
+    if (modName.isEmpty())
+    {
+        return true;
+    }
+
+    if (modName == "Shift")
+    {
+        return isShift(keys);
+    }
+
+    if (modName == "Ctrl")
+    {
+        return isControl(keys);
+    }
+
+    if (modName == "Alt")
+    {
+        return isAlt(keys);
+    }
+
+    return false;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+void ControlHandler::sendControlSignal(int vehicle_idx,
+                                       int cab_idx,
+                                       uint16_t id,
+                                       float value)
+{
+    io_control_input_t out;
+    out.controlled_vehicle_idx = vehicle_idx;
+    out.cabine_idx = cab_idx;
+    out.id = id;
+    out.value = value;
+
+    (*ctrl_inputs)[cab_idx].updateByKey1(id, out);
+    emit sigSendControlCommand(out.serialize());
+}
